38e6273d86cc2af92ab613e19e8a1692edd8866b
Robin Lovelace <rob00x@gmail.com>
2017-03-19 17:34:32 +0000

    Add comments to venn diagram code

--- code/01-venn.R
+++ code/01-venn.R
@@ -1,7 +1,7 @@
-library(sf)
-p = st_sfc(st_point(c(0, 1)), st_point(c(1, 1)))
-b = st_buffer(p, dist = 1)
-i = st_intersection(b[1], b[2])
-plot(b)
-text(x = c(-0.5, 1.5), y = 1, labels = c("Geography", "R"))
-plot(i, col = "lightgrey", add = TRUE)
+library(sf) # load sf library
+p = st_sfc(st_point(c(0, 1)), st_point(c(1, 1))) # create 2 points
+b = st_buffer(p, dist = 1) # convert points to circles
+i = st_intersection(b[1], b[2]) # find intersection between circles
+plot(b) # plot circles
+text(x = c(-0.5, 1.5), y = 1, labels = c("Geography", "R")) # add text
+plot(i, col = "lightgrey", add = TRUE) # color intersecting area
