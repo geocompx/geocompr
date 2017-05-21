8581b476c9d3db817e700dc374b347663f5eda58
jn <tupiszakaczki@gmail.com>
2017-05-21 18:19:24 -0400

    zion download

--- code/download-data.R
+++ code/download-data.R
@@ -73,11 +73,63 @@ ggplot(world_sf, aes(fill=pop)) +
 
 # yellowstone borders ---------------------------------------------
 library(sf)
-download.file("http://www.wsgs.wyo.gov/gis-files/yellowstoneboundary.zip", "data/yellowstoneboundary.zip")
-unzip("data/yellowstoneboundary.zip", exdir = "data")
-st_read('data/YellowstoneBoundary/boundary.shp') %>% 
-  st_transform(., 2246) %>% 
-  st_write('data/yellowstone.gpkg')
-file.remove('data/yellowstoneboundary.zip')
-unlink('data/YellowstoneBoundary/', recursive = TRUE)
+library(tidyverse)
+# download.file("http://www.wsgs.wyo.gov/gis-files/yellowstoneboundary.zip", "data/yellowstoneboundary.zip")
+# unzip("data/yellowstoneboundary.zip", exdir = "data")
+# st_read('data/YellowstoneBoundary/boundary.shp') %>% 
+#   # st_transform(., 2246) %>% 
+#   st_write('data/yellowstone.gpkg', delete_dsn = TRUE)
+# file.remove('data/yellowstoneboundary.zip')
+# unlink('data/YellowstoneBoundary/', recursive = TRUE)
 
+download.file("https://irma.nps.gov/DataStore/DownloadFile/570816", "data/nps_boundary.zip")
+unzip("data/nps_boundary.zip", exdir = "data")
+st_read('data/nps_boundary.shp') %>%
+  filter(PARKNAME=="Zion") %>% 
+  st_transform(., 26912) %>% 
+  st_write('data/zion.gpkg', delete_dsn = TRUE)
+
+dir(path="data", pattern="nps_boundary.*", full.names = TRUE) %>%
+  file.remove(.)
+
+# landsat -------------------------------------------------------
+# https://aws.amazon.com/public-data-sets/landsat/
+download_landsat8 <- function(destination_filename, band){
+  filename <- paste0("http://landsat-pds.s3.amazonaws.com/L8/038/034LC80380342015230LGN00/LC80380342015230LGN00_B", band, ".TIF")
+  download.file(filename, destination_filename, method='curl')
+}
+
+download_landsat8('data/zion_landsat_b2.tif', 2)
+download_landsat8('data/zion_landsat_b3.tif', 3)
+download_landsat8('data/zion_landsat_b4.tif', 4)
+download_landsat8('data/zion_landsat_b5.tif', 5)
+
+zion <- st_read('data/zion.gpkg') %>% 
+  st_buffer(., 2500)
+
+library(raster)
+
+# https://aws.amazon.com/public-data-sets/landsat/
+## other stuff
+# http://grindgis.com/blog/vegetation-indices-arcgis
+## calculate ndvi from red (band 1) and near-infrared (band 2) channel
+# Band 4 reflectance= (2.0000E-05 * (“sub_tif_Band_4”)) + -0.100000
+
+process_landsat8_ndvi <- function(input, type){
+  DN_to_radiance <- function(value){
+    value*2.0000E-05-0.1
+  }
+  r4 <- DN_to_radiance(input)
+  if (type == 1){
+    ndvi <- overlay(r4[[1]], r4[[2]], fun = function(x, y) {
+      (y-x) / (y+x)
+    })
+    writeRaster(ndvi, 'data/landsat8_ndvi_spk.tif', overwrite=TRUE)
+  } else if (type == 2){
+    savi <- overlay(r4[[1]], r4[[2]] ,fun = function(x, y) {
+      l=0.5
+      ((y-x) / (y+x+l)) * (1+l)
+    })
+    writeRaster(savi, 'data/landsat8_savi_spk.tif', overwrite=TRUE)
+  }
+}
