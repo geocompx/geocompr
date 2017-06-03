6f503768dc922e3db8b79c26e13d496f88875468
jn <tupiszakaczki@gmail.com>
2017-06-02 20:23:02 -0400

    code changes based on https://github.com/hadley/r4ds/blob/master/_common.R

--- code/before_script.R
+++ code/before_script.R
@@ -1,18 +1,17 @@
 library(methods)
-# library(geocompr)
 
 knitr::opts_chunk$set(
         comment = "#>",
         collapse = TRUE,
         cache = TRUE,
-        fig.align = "center",
         fig.pos = "t",
-        fig.path = "figures/"
+        fig.path = "figures/",
+        fig.align = 'center',
+        fig.width = 6,
+        fig.asp = 0.618,  # 1 / phi
+        fig.show = "hold"
 )
 
 set.seed(2017)
 options(digits = 3)
 options(dplyr.print_min = 4, dplyr.print_max = 4)
-
-# geocompr:::generate_chapter_code()
-
