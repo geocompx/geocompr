bf58f50a6368c4a24c810208f3ffacd3b3cd63fc
Robin Lovelace <rob00x@gmail.com>
2017-06-25 08:59:34 +0100

    Add revdep data + round it
    Former-commit-id: 5406bda460fab55a2d9785df926112b9e606f3e3

--- code/sf-revdep.R
+++ code/sf-revdep.R
@@ -3,6 +3,6 @@ sf_revdeps = devtools::revdep("sf")
 sf_revdeps_dls = cranlogs::cran_downloads(packages = sf_revdeps, when = "last-month")
 top_dls = sf_revdeps_dls %>% 
   group_by(package) %>% 
-  summarise(Downloads = mean(count), date = max(date)) %>% 
+  summarise(Downloads = round(mean(count)), date = max(date)) %>% 
   arrange(desc(Downloads))
 write_csv(x = top_dls, path = "extdata/top_dls.csv")
