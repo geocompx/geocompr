58cae905fc174985cd4004c4ddc75089fb47fcb1
Robin Lovelace <rob00x@gmail.com>
2017-07-15 00:05:22 +0100

    Add code to create figure of globe
    Former-commit-id: af8b65507df4b5007f9e8a60a20fe0cf36ce815c

---
+++ code/globe.R
@@ -0,0 +1,10 @@
+# Aim: plot globe
+if(!require(globe)) {
+  install.packages("globe")
+}
+library(sf)
+center = st_sf(st_sfc(st_point(c(0, 0))))
+buf_equator = st_buffer(x = center, dist = 10)
+coords = st_coordinates(buf_equator)[, 1:2]
+globeearth(eye = c(0, 0))
+globelines(loc = coords)
