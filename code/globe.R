c43b704fd38b952d4765e0a22a78c59d1e23f579
Robin Lovelace <rob00x@gmail.com>
2017-07-15 00:32:38 +0100

    Fix name of buff_ object

--- code/globe.R
+++ code/globe.R
@@ -4,7 +4,7 @@ if(!require(globe)) {
 }
 library(sf)
 center = st_sf(st_sfc(st_point(c(0, 0))))
-buf_equator = st_buffer(x = center, dist = 10)
+buff_equator = st_buffer(x = center, dist = 10)
 coords = st_coordinates(buf_equator)[, 1:2]
 png(filename = "figures/globe.png", width = 500, height = 500)
 globeearth(eye = c(0, 0))
