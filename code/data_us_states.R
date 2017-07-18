6167b12ff67ddb5deda2746073a44800d52118c9
jn <tupiszakaczki@gmail.com>
2017-07-17 21:05:44 -0400

    non-spatial data us states creation
    Former-commit-id: ccf2663c675039d992e0a4862e0b519629272942

--- code/data_us_states.R
+++ code/data_us_states.R
@@ -10,20 +10,15 @@ options(tigris_class = "sf")
 ## census data
 census_api_key("YOUR API KEY") # http://api.census.gov/data/key_signup.html
 
-# v15 <- load_variables(2015, "acs5", cache = TRUE) 
+# v15 = load_variables(2015, "acs5", cache = TRUE)
 # View(v15)
+# v15 %>% select(concept) %>% unique() %>% View
 # B01003_001E - total pop
-# B06011_001E - Median income in the past 12 months --!!Total:
 
-total_pop_10 <- get_acs(geography = "state", variables = "B01003_001E", year = 2010) %>% 
+total_pop_10 = get_acs(geography = "state", variables = "B01003_001E", year = 2010) %>% 
   select(GEOID, total_pop_10 = estimate)
-total_pop_15 <- get_acs(geography = "state", variables = "B01003_001E", year = 2015) %>% 
+total_pop_15 = get_acs(geography = "state", variables = "B01003_001E", year = 2015) %>% 
   select(GEOID, total_pop_15 = estimate)
-
-median_income_10 <- get_acs(geography = "state", variables = "B06011_001E", year = 2010) %>% 
-  select(GEOID, median_income_10 = estimate)
-median_income_15 <- get_acs(geography = "state", variables = "B06011_001E", year = 2015) %>% 
-  select(GEOID, median_income_15 = estimate)
 
 ## groups - Census Bureau-designated regions
 ## spatial data 
@@ -35,8 +30,23 @@ us_states49 = us_states %>%
   mutate(REGION = factor(REGION, labels = c("Norteast", "Midwest", "South", "West"))) %>% 
   mutate(AREA = units::set_units(st_area(.), km^2)) %>% 
   left_join(., total_pop_10, by = "GEOID") %>% 
-  left_join(., total_pop_15, by = "GEOID") %>% 
-  left_join(., median_income_10, by = "GEOID") %>% 
-  left_join(., median_income_15, by = "GEOID") 
+  left_join(., total_pop_15, by = "GEOID")
   
 plot(us_states49["REGION"])
+
+## non-spatial data
+# B06011_001E - Median income in the past 12 months --!!Total:
+median_income_10 = get_acs(geography = "state", variables = "B06011_001E", year = 2010) %>% 
+  select(NAME, median_income_10 = estimate)
+median_income_15 = get_acs(geography = "state", variables = "B06011_001E", year = 2015) %>% 
+  select(NAME, median_income_15 = estimate)
+# B17001_002E - Income in the past 12 months below poverty level:
+poverty_level_10 = get_acs(geography = "state", variables = "B17001_002E", year = 2010) %>% 
+  select(NAME, poverty_level_10 = estimate)
+poverty_level_15 = get_acs(geography = "state", variables = "B17001_002E", year = 2015) %>% 
+  select(NAME, poverty_level_15 = estimate)
+
+us_state_eco = median_income_10 %>% 
+  left_join(median_income_15, by = "NAME") %>% 
+  left_join(poverty_level_10, by = "NAME") %>% 
+  left_join(poverty_status_15, by = "NAME")
