5957f302c6176ab56802f1886e4f34e06caaeb48
jannes-m <malnamalja@gmx.de>
2017-09-06 12:58:34 +0200

    renaming raster objects in the code

--- code/create-rasters.R
+++ code/create-rasters.R
@@ -1,9 +1,10 @@
-r = raster(nrow = 6, ncol = 6, res = 0.5, 
+library("raster")
+elev = raster(nrow = 6, ncol = 6, res = 0.5, 
            xmn = -1.5, xmx = 1.5, ymn = -1.5, ymx = 1.5,
            vals = 1:36)
 
 grain_size = c("clay", "silt", "sand")
-r_2 = raster(nrow = 6, ncol = 6, res = 0.5, 
-             xmn = -1.5, xmx = 1.5, ymn = -1.5, ymx = 1.5,
-             vals = factor(sample(grain_size, 36, replace = TRUE), 
-                           levels = grain_size))
+grain = raster(nrow = 6, ncol = 6, res = 0.5, 
+               xmn = -1.5, xmx = 1.5, ymn = -1.5, ymx = 1.5,
+               vals = factor(sample(grain_size, 36, replace = TRUE), 
+                             levels = grain_size))
