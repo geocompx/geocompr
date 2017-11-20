2d7238b9d99076aa2cf10bdbb93244cebfb29897
jannes-m <malnamalja@gmx.de>
2017-11-20 17:33:05 +0100

    Adjusting raster intro plot (show NAs)
    Former-commit-id: b53475b1625ffcfb485b1fde0b49deeac637eadd

--- code/02_raster_intro_plot.R
+++ code/02_raster_intro_plot.R
@@ -1,49 +1,79 @@
+library(raster)
 library(ggplot2)
 library(visualraster)
 library(gridExtra)
 library(spData)
 library(spDataLarge)
+library(lattice)
+library(latticeExtra)
 
 theme_set(theme_fullframe())
 
 # first intro plot -----------------------------------------------------------
 set.seed(2017-04-01)
 
-small_ras = raster(matrix(1:16, 4, 4, byrow =TRUE))
+small_ras = raster(matrix(1:16, 4, 4, byrow = TRUE))
 small_ras_val = raster(matrix(sample.int(100, 16), 4, 4, byrow =TRUE))
-NAvalue(small_ras_val) <- -9999
-small_ras_val[c(7, 9)] = -9999
-small_ras_val_2 <- small_ras_val
-small_ras_val_2[c(7, 9)] <- NA
+# NAvalue(small_ras_val) <- NA
+small_ras_val[c(7, 9)] = NA
+# small_ras_val_2 <- small_ras_val
+# small_ras_val_2[c(7, 9)] <- NA
 
 # empty_grid_plot = ggplot() +
 #   vr_geom_raster_seq(small_ras) +
 #   scale_fill_gradientn(colors = c("white")) +
 #   labs(title = "A. Raster grid")
 
-cells_num_plot = ggplot() +
-  vr_geom_raster_seq(small_ras) +
-  vr_geom_text(small_ras) + 
-  scale_fill_gradientn(colors = c("white")) +
-  labs(title = "A. Cell IDs")
+polys = rasterToPolygons(small_ras, na.rm = FALSE)
+# cell IDs
+p_1 = spplot(small_ras, colorkey = FALSE, col.regions = "white",
+             main = "A. Cell IDs",
+             sp.layout = list(
+               list("sp.polygons", polys, first = FALSE),
+               list("sp.text", xyFromCell(small_ras_val, 1:ncell(small_ras)),
+                    1:ncell(small_ras))
+             )
+)
+# cell values
+p_2 = spplot(small_ras_val, colorkey = FALSE, col.regions = "white",
+             main = "B. Cell values",
+             sp.layout = list(
+               list("sp.polygons", polys, first = FALSE),
+               list("sp.text", xyFromCell(small_ras_val,
+                                          1:ncell(small_ras_val)),
+                    values(small_ras_val))
+             )
+)
+# color map
+p_3 = spplot(small_ras_val, 
+             col.regions = colorRampPalette(c("#a50026", "#ffffbf", "#006837"))(16),
+             colorkey = FALSE,
+             main = "C. Colored cell values")
 
-cells_val_plot = ggplot() +
-  vr_geom_raster_seq(small_ras_val) +
-  vr_geom_text(small_ras_val) + 
-  scale_fill_gradientn(colors = c("white")) +
-  labs(title = "B. Cell values")
 
-map_plot = ggplot() +
-  vr_geom_raster_seq(small_ras_val_2) +
-  # vr_geom_text(small_ras_val) +
-  labs(title = "C. Coloring cell values") +
-  scale_fill_gradientn(colours=c("#a50026", "#ffffbf", "#006837"))
+# cells_num_plot = ggplot() +
+#   vr_geom_raster_seq(small_ras) +
+#   vr_geom_text(small_ras) + 
+#   scale_fill_gradientn(colors = c("white")) +
+#   labs(title = "A. Cell IDs")
+# 
+# cells_val_plot = ggplot() +
+#   vr_geom_raster_seq(small_ras_val) +
+#   vr_geom_text(small_ras_val) + 
+#   scale_fill_gradientn(colors = c("white")) +
+#   labs(title = "B. Cell values")
+# 
+# map_plot = ggplot() +
+#   vr_geom_raster_seq(small_ras_val_2) +
+#   # vr_geom_text(small_ras_val) +
+#   labs(title = "C. Coloring cell values") +
+#   scale_fill_gradientn(colours = c("#a50026", "#ffffbf", "#006837"))
 
-raster_intro_plot = arrangeGrob(cells_num_plot,
-                                cells_val_plot, map_plot, 
+raster_intro_plot = arrangeGrob(p_1, p_2, p_3,
                                 ncol = 3)
 
-ggsave(plot = raster_intro_plot, filename = "figures/02_raster_intro_plot.png", width = 6, height = 3, scale = 1.25)
+ggsave(plot = raster_intro_plot, filename = "figures/02_raster_intro_plot.png",
+       width = 6, height = 3, scale = 1.25)
 
 # second intro plot -----------------------------------------------------------
 library(rasterVis)
