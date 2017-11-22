68dee79d23d3f307280b6ccd9c34e86a360272e6
Robin Lovelace <rob00x@gmail.com>
2017-11-22 21:49:56 +0000

    Finish first draft of congruence example
    Former-commit-id: d9aa06359fee744460e425af765b613987dec22e

--- code/04-congruence.R
+++ code/04-congruence.R
@@ -20,9 +20,34 @@ ed_cents = ed_cents[aggzones, ]
 incongruent = ed1981[ed_cents, ]
 
 # Bind the two types of shape together
+congruent$layer = "Congruent"
+congruent = congruent["layer"]
+congruent$value = "blue"
+incongruent$layer = "Incongruent"
+incongruent = incongruent["layer"]
+incongruent$value = "yellow"
+rx = rbind(congruent, incongruent)
+tmap_mode("plot")
+m = qtm(rx, "value", borders = "black") + tm_facets(by = "layer", drop.units = TRUE, ncol = 2) +
+  tm_shape(aggzones) +
+  tm_borders(alpha = 0.6, lwd = 10) +
+  tm_layout(legend.show = FALSE)
+detach("package:tmap", unload = TRUE)
+save_tmap(m, "figures/04-congruence.png")
+# test visuals ----
+# tmap_mode("view")
+# qtm(aggzones, borders = "black") +
+#   qtm(congruent, fill = "blue", borders = "grey") +
+#   qtm(incongruent, fill = "yellow", borders = "white")
 
+# # old solution ----
+# rx = st_as_sf(raster::rasterToPolygons(raster::raster(ncol = 4, nrow = 4)))
+# ry = st_as_sf(raster::rasterToPolygons(raster::raster(ncol = 2, nrow = 2)))
+# rxo = st_as_sf(st_set_geometry(rx, NULL), geometry = rx$geometry + 15)
+# st_crs(rx) = NA
+# rx = rbind(rx, rxo)
+# set.seed(1985)
+# rx$value = rep(runif(nrow(rx) / 2), 2)
+# rx$layer = rep(c("Congruent", "Incongruent"), each = nrow(rxo))
+# library(tmap)
 
-tmap_mode("view")
-qtm(aggzones, borders = "black") +
-  qtm(congruent, fill = "blue", borders = "grey") +
-  qtm(incongruent, fill = "yellow", borders = "white")
