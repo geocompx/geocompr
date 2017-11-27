44f4273ed1ae0dec46a8f33e9af7203d399344d5
Robin Lovelace <rob00x@gmail.com>
2017-11-27 13:37:38 +0000

    Fix #110
    Former-commit-id: 31ed9e010789ed86297bdad86e7ed4f8955d5354
    Former-commit-id: 95850e803bfd58f8f2138efcd02a47eb46695e38

--- code/04-congruence.R
+++ code/04-congruence.R
@@ -1,39 +1,16 @@
 # Aim: generate plot to show the concept of spatial congruence
-devtools::install_github("robinlovelace/ukboundaries")
 library(sf)
 library(tmap)
-library(ukboundaries)
-# data(package = "ukboundaries") # see datasets available
-sel_wetherby = grepl("001|002", msoa2011_lds$geo_label)
-aggzones = st_transform(msoa2011_lds[sel_wetherby, ], 27700)
-
-# find lsoas in the aggzones (there must be a neater way...)
-lsoa_touching = st_transform(lsoa2011_lds, 27700)[aggzones, ]
-lsoa_cents = st_centroid(lsoa_touching)
-lsoa_cents = lsoa_cents[aggzones, ]
-congruent = lsoa_touching[lsoa_cents, ]
-
-# same for ed zones
-ed_touching = st_transform(ed1981, 27700)[aggzones, ]
-ed_cents = st_centroid(ed_touching)
-ed_cents = ed_cents[aggzones, ]
-incongruent = ed_touching[ed_cents, ]
-
-# Bind the two types of shape together
-incongruent$layer = "Incongruent"
-incongruent = incongruent["layer"]
-set.seed(2017)
-incongruent$value = rnorm(nrow(incongruent), mean = 5)
-incongruent_cents = st_centroid(incongruent)
-congruent$layer = "Congruent"
-congruent = congruent["layer"]
-congruent$value = aggregate(incongruent_cents["value"], congruent, mean)$value
+library(spData)
 rx = rbind(congruent, incongruent)
 tmap_mode("plot")
-m = qtm(rx, "value", borders = "black") + tm_facets(by = "layer", drop.units = TRUE, ncol = 2) +
-  tm_shape(aggzones) +
-  tm_borders(alpha = 0.6, lwd = 10) +
-  tm_layout(legend.show = FALSE, scale = 0.7)
+m = tm_shape(rx) +
+  tm_fill("value", breaks = seq(4, 6, by = 0.5)) +
+  tm_borders(lwd = 2, col = "black") +
+  tm_facets(by = "level", drop.units = TRUE, ncol = 2) +
+  tm_shape(aggregating_zones) +
+  tm_borders(alpha = 0.5, lwd = 10) +
+  tm_layout(legend.show = TRUE, scale = 0.3)
 save_tmap(m, "figures/04-congruence.png", width = 1000, height = 400)
 detach("package:tmap", unload = TRUE)
 # test visuals ----
