8526bda78ae9938be516f71c17b5d6261eb4819b
robinlovelace <rob00x@gmail.com>
2018-01-09 09:58:48 +0000

    Add more code into code/
    Former-commit-id: 8569464607e7cea6d2574a47abcbacca49a0149b

---
+++ code/05-raster-vectorization2.R
@@ -0,0 +1,27 @@
+library(tmap)
+library(grid)
+cols = c("sand" = "rosybrown", "clay" = "brown", "silt" = "sandybrown")
+
+p1p = tm_shape(grain) + 
+  tm_raster(legend.show = FALSE, palette = cols) +
+  tm_layout(outer.margins = rep(0.01, 4), 
+            inner.margins = rep(0, 4))
+
+p2p = tm_shape(grain_poly) + 
+  tm_polygons("layer", legend.show = FALSE, palette = cols, lwd = 3) +
+  tm_layout(outer.margins = rep(0.01, 4), 
+            inner.margins = rep(0, 4))
+
+p3p = tm_shape(grain_poly2) + 
+  tm_polygons("layer", legend.show = FALSE, palette = cols, lwd = 3) +
+  tm_layout(outer.margins = rep(0.01, 4), 
+            inner.margins = rep(0, 4))
+
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 3, heights = unit(c(0.25, 5), "null"))))
+grid.text("raster", vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+grid.text("polygons", vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
+grid.text("aggregated polygons", vp = viewport(layout.pos.row = 1, layout.pos.col = 3))
+print(p1p, vp=viewport(layout.pos.col = 1))
+print(p2p, vp=viewport(layout.pos.col = 2))
+print(p3p, vp=viewport(layout.pos.col = 3))
---
+++ code/05-vector-rasterization2.R
@@ -0,0 +1,30 @@
+library(tmap)
+library(grid)
+
+california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
+
+r1po = tm_shape(california_raster1) +
+  tm_raster(legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) + 
+  tm_dots() + 
+  tm_shape(california) +
+  tm_borders() + 
+  tm_layout(outer.margins = rep(0.01, 4), 
+            inner.margins = rep(0, 4))
+
+r2po = tm_shape(california_raster2) +
+  tm_raster(legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) + 
+  tm_dots() + 
+  tm_shape(california) +
+  tm_borders() + 
+  tm_layout(outer.margins = rep(0.01, 4), 
+            inner.margins = rep(0, 4))
+
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 2, heights = unit(c(0.5, 5), "null"))))
+
+grid.text("A. Line rasterization", vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+print(r1po, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
+grid.text("B. Polygon rasterization", vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
+print(r2po, vp = viewport(layout.pos.row = 2, layout.pos.col = 2))
