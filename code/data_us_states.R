2ccd4f6818d4a6d10350ebd681395c12c02da43f
jn <tupiszakaczki@gmail.com>
2018-02-03 11:45:29 -0500

    hawaii and alaska download
    Former-commit-id: 9eeeb991c59b4f50ed4e47bd67c9fdd30b224b93

--- code/data_us_states.R
+++ code/data_us_states.R
@@ -9,7 +9,7 @@ library(units)
 library(rmapshaper)
 options(tigris_class = "sf")
 
-## census data
+# census data -------------------------------------------------------------
 census_api_key("YOUR API KEY") # http://api.census.gov/data/key_signup.html
 
 # v15 = load_variables(2015, "acs5", cache = TRUE)
@@ -23,9 +23,11 @@ total_pop_15 = get_acs(geography = "state", variables = "B01003_001E", endyear =
   select(GEOID, total_pop_15 = estimate)
 
 ## groups - Census Bureau-designated regions
-## spatial data 
+
+# spatial data  -----------------------------------------------------------
 us_states = states() 
 
+# cont states -------------------------------------------------------------
 us_states49 = us_states %>% 
   filter(DIVISION != 0) %>% 
   filter(NAME != "Alaska", NAME != "Hawaii") %>% 
@@ -41,7 +43,34 @@ us_states49 = us_states %>%
 us_states = us_states49
 save(us_states, file = "data/us_states.rda")
 
-## non-spatial data
+# hawaii ------------------------------------------------------------------
+hawaii = us_states %>% 
+  filter(DIVISION != 0) %>% 
+  filter(NAME == "Hawaii") %>% 
+  ms_simplify(keep = 0.05, keep_shapes = TRUE, explode = TRUE) %>% 
+  aggregate(by = list(.$GEOID), first) %>% 
+  select(GEOID, NAME, REGION) %>%
+  mutate(REGION = factor(REGION, labels = c("West"))) %>% 
+  mutate(AREA = units::set_units(st_area(.), km^2)) %>% 
+  left_join(., total_pop_10, by = "GEOID") %>% 
+  left_join(., total_pop_15, by = "GEOID")
+
+save(hawaii, file = "data/hawaii.rda", compress = "bzip2")
+# alaska ------------------------------------------------------------------
+alaska = us_states %>% 
+  filter(DIVISION != 0) %>% 
+  filter(NAME == "Alaska") %>% 
+  ms_simplify(keep = 0.05, keep_shapes = TRUE, explode = TRUE) %>% 
+  aggregate(by = list(.$GEOID), first) %>% 
+  select(GEOID, NAME, REGION) %>%
+  mutate(REGION = factor(REGION, labels = c("West"))) %>% 
+  mutate(AREA = units::set_units(st_area(.), km^2)) %>% 
+  left_join(., total_pop_10, by = "GEOID") %>% 
+  left_join(., total_pop_15, by = "GEOID")
+
+save(alaska, file = "data/alaska.rda", compress = "bzip2")
+
+# non-spatial data --------------------------------------------------------
 # B06011_001E - Median income in the past 12 months --!!Total:
 median_income_10 = get_acs(geography = "state", variables = "B06011_001E", endyear = 2010) %>% 
   select(NAME, median_income_10 = estimate)
