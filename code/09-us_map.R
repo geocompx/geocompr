a55787f49a41cf440ba1687066e1f104f248a6cb
jn <tupiszakaczki@gmail.com>
2018-02-17 10:42:42 -0500

    new usa layout
    Former-commit-id: fa5133796de474227f89bf978882872edb1475db

--- code/09-us_map.R
+++ code/09-us_map.R
@@ -2,6 +2,7 @@ library(sf)
 library(spData)
 library(tidyverse)
 library(tmap)
+library(grid)
 
 us_states2163 = st_transform(us_states, 2163)
 
@@ -9,8 +10,8 @@ us_states_area = st_area(st_as_sfc(st_bbox(us_states2163)))
 hawaii_area = st_area(st_as_sfc(st_bbox(hawaii)))
 alaska_area = st_area(st_as_sfc(st_bbox(alaska)))
 
-us_states_hawaii_ratio = hawaii_area/us_states_area
-us_states_alaska_ratio = alaska_area/us_states_area
+us_states_hawaii_ratio = as.numeric(hawaii_area/us_states_area)
+us_states_alaska_ratio = as.numeric(alaska_area/us_states_area)
 
 us_states_map = tm_shape(us_states2163) +
   tm_polygons() + 
@@ -24,15 +25,10 @@ alaska_map = tm_shape(alaska) +
   tm_polygons() + 
   tm_layout(title = "Alaska", frame = FALSE, bg.color = NA)
 
-
-library(grid)
-
-us_states_map
-
-print(hawaii_map, vp = viewport(x = 0.4, y = 0.1,
-                                width = us_states_hawaii_ratio,
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 1, heights = unit(c(us_states_alaska_ratio, 1), "null"))))
+print(alaska_map, vp = viewport(layout.pos.row = 1))
+print(us_states_map, vp = viewport(layout.pos.row = 2))
+print(hawaii_map, vp = viewport(x = 0.25, y = 0.1,
                                 height = us_states_hawaii_ratio))
 
-print(alaska_map, vp = viewport(x = 0.5, y = 0.8, 
-                                width = us_states_alaska_ratio, 
-                                height = us_states_alaska_ratio))
