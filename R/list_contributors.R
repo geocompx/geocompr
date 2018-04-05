c52f4160dfc9a5087a7680b4c9eb7cbd57b70f25
Robin Lovelace <rob00x@gmail.com>
2018-04-05 15:28:05 +0100

    Revert "Comment-out gh:: code to prevent build issues"
    This reverts commit 344304d5f07d83e429d54bc577bcb777e9241567.

--- R/list_contributors.R
+++ R/list_contributors.R
@@ -1,6 +1,6 @@
 # Filename: list_contributors.R (2018-03-16)
 #
-# Note: commented-out to prevent build issues
+# TO DO: List all geocompr contributors
 #
 # Author(s): Jannes Muenchow & Robin Lovelace
 #
@@ -15,19 +15,19 @@
 # 1 ATTACH PACKAGES AND DATA-------------------------------
 #**********************************************************
 
-# # attach packages
-# library(gh)
-# library(tidyverse)
-# 
-# #**********************************************************
-# # 2 CONTRIBUTOR LIST---------------------------------------
-# #**********************************************************
-# 
-# # git has to be in PATH
-# out_json = gh::gh(endpoint = "/repos/robinlovelace/geocompr/contributors")
-# link = vapply(out_json, "[[", FUN.VALUE = "", "html_url")
-# name = gsub(pattern = "https://github.com/", "", link)
-# commits = paste0("https://github.com/Robinlovelace/geocompr/commits?author=", name)
-# out_df = data_frame(name, link)
-# # remove book authors
-# filter(out_df, !grepl("robin|jannes|jn|jakub|nowosad", name, TRUE)) 
+# attach packages
+library(gh)
+library(tidyverse)
+
+#**********************************************************
+# 2 CONTRIBUTOR LIST---------------------------------------
+#**********************************************************
+
+# git has to be in PATH
+out_json = gh::gh(endpoint = "/repos/robinlovelace/geocompr/contributors")
+link = vapply(out_json, "[[", FUN.VALUE = "", "html_url")
+name = gsub(pattern = "https://github.com/", "", link)
+commits = paste0("https://github.com/Robinlovelace/geocompr/commits?author=", name)
+out_df = data_frame(name, link)
+# remove book authors
+filter(out_df, !grepl("robin|jannes|jn|jakub|nowosad", name, TRUE)) 
