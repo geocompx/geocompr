80ba7e77540e20f6d46d731ea0d7f2e2746b72a7
Robin Lovelace <rob00x@gmail.com>
2018-04-09 12:43:42 +0100

    Change directory of chapter code
    Former-commit-id: aa360d9683572d6b5217ceaf45009437f9f5df5d
    Former-commit-id: 194f0b2aedb0ee82ef34e9b10da301fb069df0b2

--- R/generate-chapter-code.R
+++ R/generate-chapter-code.R
@@ -1,6 +1,6 @@
 #' Extracts R code from each chapter and dumps it in the code folder
 #'
-generate_chapter_code = function(dir = ".", out_dir  = "code/") {
+generate_chapter_code = function(dir = ".", out_dir  = "code/chapters/") {
         rmd_files = list.files(path = dir, pattern = ".Rmd")
         r_files = paste0(out_dir, rmd_files)
         r_files = gsub(pattern = "Rmd", replacement = "R", r_files)
--- code/index.R
+++
@@ -1,9 +0,0 @@
-## ---- echo=FALSE, include=FALSE------------------------------------------
-# source("code/before_script.R")
-
-## ---- eval=FALSE---------------------------------------------------------
-#> install.packages("sf") # for the stable version
-
-## ---- eval=FALSE---------------------------------------------------------
-#> devtools::install_github("edzer/sfr") # for the development version
-
