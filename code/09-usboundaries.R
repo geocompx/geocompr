ba686ef1bc596eab313d4db01fa199fdda53e1d8
Robin Lovelace <rob00x@gmail.com>
2018-04-09 16:57:03 +0100

    Improve description of animated maps
    Former-commit-id: e02bd2693a888913e36daf20291564aa8b688e49
    Former-commit-id: 7924989a3d5c00ed057edd9b6757a13dc0e15a1c

--- code/09-usboundaries.R
+++ code/09-usboundaries.R
@@ -6,3 +6,14 @@ library(USAboundaries)
 dates = lubridate::as_date(unique(historydata::us_state_populations$year))
 # USAboundaries::us_boundaries(map_date = dates[1])
 # ...
+library(tmap)
+statepop = historydata::us_state_populations %>%
+  dplyr::select(-GISJOIN) %>% rename(NAME = state)
+statepop_wide = spread(statepop, year, population, sep = "_")
+statepop_sf = left_join(spData::us_states, statepop_wide) %>% 
+  st_transform(2163)
+# map_dbl(statepop_sf, ~sum(is.na(.)))  # looks about right
+year_vars = names(statepop_sf)[grepl("year", names(statepop_sf))]
+facet_anim = tm_shape(statepop_sf) + tm_fill(year_vars) + tm_facets(free.scales.fill = FALSE, 
+                                                                    ncol = 1, nrow = 1)
+animation_tmap(tm = facet_anim, filename = "figures/09-us_pop.gif")
