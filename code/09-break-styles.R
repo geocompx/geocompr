f1b291940446371f665a1398cdb899e62a829fff
jn <tupiszakaczki@gmail.com>
2018-04-14 12:41:22 -0400

    nz plot examples fixed
    Former-commit-id: 8b8f07fd94e5856981ecee3150ca6f7c14f19725
    Former-commit-id: 1c5e11311a1efb8e08eaad88b40faf2093def974

--- code/09-break-styles.R
+++ code/09-break-styles.R
@@ -1,30 +1,30 @@
 library(tmap)
 library(spData)
 # breaks = c(0, 3, 4, 5) * 10000
-# m_fixed = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "fixed", breaks = breaks) +
+# m_fixed = tm_shape(nz) + tm_polygons(col = "Median_income", style = "fixed", breaks = breaks) +
 #   tm_layout(title = 'style = "fixed"', title.size = 0.7)    
-# m_sd = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "sd") +                       
+# m_sd = tm_shape(nz) + tm_polygons(col = "Median_income", style = "sd") +                       
   # tm_layout(title = 'style = "sd"', title.size = 0.7)
-m_equal = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "equal") +                 
+m_equal = tm_shape(nz) + tm_polygons(col = "Median_income", style = "equal") +                 
   tm_layout(title = 'style = "equal"', title.size = 0.7)
-m_pretty = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "pretty") +               
+m_pretty = tm_shape(nz) + tm_polygons(col = "Median_income", style = "pretty") +               
   tm_layout(title = 'style = "pretty"', title.size = 0.7)        
-m_quantile = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "quantile") +           
+m_quantile = tm_shape(nz) + tm_polygons(col = "Median_income", style = "quantile") +           
   tm_layout(title = 'style = "quantile"', title.size = 0.7)
-# m_kmeans = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "kmeans") +               
+# m_kmeans = tm_shape(nz) + tm_polygons(col = "Median_income", style = "kmeans") +               
 #   tm_layout(title = 'style = "kmeans"', title.size = 0.7)
-# m_hclust = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "hclust") +               
+# m_hclust = tm_shape(nz) + tm_polygons(col = "Median_income", style = "hclust") +               
 #   tm_layout(title = 'style = "hclust"', title.size = 0.7)
-# m_bclust = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "bclust") +               
+# m_bclust = tm_shape(nz) + tm_polygons(col = "Median_income", style = "bclust") +               
 #   tm_layout(title = 'style = "bclust"', title.size = 0.7)
-# m_fisher = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "fisher") +               
+# m_fisher = tm_shape(nz) + tm_polygons(col = "Median_income", style = "fisher") +               
 #   tm_layout(title = 'style = "fisher"', title.size = 0.7)
-m_jenks = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "jenks") +                 
+m_jenks = tm_shape(nz) + tm_polygons(col = "Median_income", style = "jenks") +                 
   tm_layout(title = 'style = "jenks"', title.size = 0.7)
 m_cont = tm_shape(nz_elev) + tm_raster(col = "elevation", style = "cont") +                   
   tm_layout(title = 'style = "cont"', title.size = 0.7)
-# m_order = tm_shape(nz) + tm_polygons(col = "AREA_SQ_KM", style = "order") +                 
+# m_order = tm_shape(nz) + tm_polygons(col = "Median_income", style = "order") +                 
   # tm_layout(title = 'style = "order"', title.size = 0.7)
-m_cat = tm_shape(nz) + tm_polygons(col = "REGC2017_NAME", style = "cat") +                  
+m_cat = tm_shape(nz) + tm_polygons(col = "Island", style = "cat") +                  
   tm_layout(title = 'style = "cat"', title.size = 0.7)          
 tmap_arrange(m_pretty, m_equal, m_quantile, m_jenks, m_cont, m_cat) 
--- code/09-tmpal.R
+++ code/09-tmpal.R
@@ -1,8 +1,8 @@
 library(tmap)
 library(spData)
 breaks = c(0, 3, 4, 5) * 1e4
-mc1 = tm_shape(nz) + tm_fill(col = "AREA_SQ_KM")
-mc2 = tm_shape(nz) + tm_fill(col = "AREA_SQ_KM", breaks = breaks)
-mc3 = tm_shape(nz) + tm_fill(col = "AREA_SQ_KM", n = 10)
-mc4 = tm_shape(nz) + tm_fill(col = "AREA_SQ_KM", palette = "RdBu")
+mc1 = tm_shape(nz) + tm_fill(col = "Median_income")
+mc2 = tm_shape(nz) + tm_fill(col = "Median_income", breaks = breaks)
+mc3 = tm_shape(nz) + tm_fill(col = "Median_income", n = 10)
+mc4 = tm_shape(nz) + tm_fill(col = "Median_income", palette = "RdBu")
 tmap_arrange(mc1, mc2, mc3, mc4, nrow = 1)
