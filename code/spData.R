bd3408861b4e7f29d7ec308f5ee8f3d3c505526e
Robin Lovelace <rob00x@gmail.com>
2018-04-25 17:23:45 +0100

    Reproducibility: show install.packages code
    Aimed at new users not used to installing packages. Also allows the book to be reproduced if spDataLarge cannot be installed - e.g. on managed computers

---
+++ code/spData.R
@@ -0,0 +1,7 @@
+# Aim load data from spDataLarge if you cannot install the package
+if(!dir.exists("spDataLarge-master/")) {
+  download.file("https://github.com/Nowosad/spDataLarge/archive/master.zip", "spDataLarge.zip")
+  unzip("spDataLarge.zip")
+}
+files_rda = list.files("spDataLarge-master/data/", full.names = TRUE)
+sapply(files_rda, load, envir = .GlobalEnv)
