7e1bf85b18bd38a60a70384780d883f3cc16e9ce
Robin Lovelace <rob00x@gmail.com>
2018-05-17 23:19:17 +0100

    Fix #234

--- code/04-spatial-join.R
+++ code/04-spatial-join.R
@@ -1,12 +1,39 @@
-par(mfrow = c(1, 3), mar = c(0, 0, 1, 0))
-plot(urb$geometry, col = "white", main = "Target object")
-plot(asia$geom, add = TRUE)
-plot(urb$geometry, col = "white", main = "Source object (points)")
-plot(urb["population_millions"], pch = 15, key.pos = NULL, add = TRUE, pal = rainbow)
-plot(asia$geom, border = "grey", add = TRUE)
-par(mar = c(0, 0, 1, 0))
-plot(urb$geometry, col = "white", main = "Result of spatial join")
-plot(urb["population_millions"], pch = 15, key.pos = NULL, add = TRUE, pal = rainbow)
-plot(asia$geom, add = TRUE)
-plot(joined["population_millions"], add = TRUE, key.pos = NULL, pal = rainbow)
-par(mfrow = c(1, 1))
+# Aim: demonstrate spatial joins ------------------------------------------
+library(sf)
+library(spData)
+library(tmap)
+# names(world)
+# names(urban_agglomerations)
+# Question arising from the data:
+# what % of country populations lived in their largest agglomorations?
+# explanation: we're joining the point data onto world
+
+if(!exists("random_joined")) {
+  set.seed(2018)
+  bb_world = st_bbox(world)
+  random_df = tibble(
+    x = runif(n = 10, min = bb_world[1], max = bb_world[3]),
+    y = runif(n = 10, min = bb_world[2], max = bb_world[4])
+  )
+  random_points = st_as_sf(random_df, coords = c("x", "y")) %>% 
+    st_set_crs(4326)
+  
+  world_random = world[random_points, ]
+  random_joined = st_join(random_points, world["name_long"])
+  
+}
+jm0 = tm_shape(world) +
+  tm_borders()
+jm1 = jm0 +
+  tm_shape(shp = random_points, bbox = bb_world) +
+  tm_symbols(col = "black", shape = 4, border.lwd = 2) +
+  tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
+jm2 = jm0 +
+  tm_shape(world_random, bbox = bb_world) +
+  tm_polygons(col = "name_long", border.col = "black", lwd = 2, palette = "Dark2") +
+  # tm_borders(col = "name_long", lwd = 4) + # issue with tmap: no variable border
+  tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
+jm3 = jm0 +
+  tm_shape(shp = random_joined, bbox = bb_world) +
+  tm_symbols(col = "name_long", shape = 4, border.lwd = 2, palette = "Dark2") +
+  tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
