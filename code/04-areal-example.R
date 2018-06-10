d558e42d2536a15652bd801bb0a45a09e0736f83
jn <tupiszakaczki@gmail.com>
2018-06-10 12:29:56 -0400

    print added

--- code/04-areal-example.R
+++ code/04-areal-example.R
@@ -3,15 +3,15 @@ library(sf)
 library(tmap)
 library(spData)
 rx = rbind(congruent, incongruent)
-tmap_mode("plot")
+# tmap_mode("plot")
 m = tm_shape(rx) +
   tm_fill("value", breaks = seq(4, 6, by = 0.5)) +
   tm_borders(lwd = 2, col = "black", lty = 1) +
-  tm_facets(by = "level", drop.units = TRUE, ncol = 2) +
+  tm_facets(by = "level", drop.units = TRUE, ncol = 1) +
   tm_shape(aggregating_zones) +
   tm_borders(alpha = 0.4, lwd = 10, col = "blue") +
   tm_layout(legend.show = TRUE, scale = 1)
-m
+print(m)
 # tmap_save(m, f, width = 1000, height = 400)
 # f = "figures/04-congruence.png"
 # i = magick::image_read(f)
