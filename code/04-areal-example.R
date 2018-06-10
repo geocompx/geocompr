f365d8704e5af768a27363e3a363354b5a726852
jn <tupiszakaczki@gmail.com>
2018-06-10 12:17:12 -0400

    areal-example map imprv test

--- code/04-areal-example.R
+++ code/04-areal-example.R
@@ -10,16 +10,16 @@ m = tm_shape(rx) +
   tm_facets(by = "level", drop.units = TRUE, ncol = 2) +
   tm_shape(aggregating_zones) +
   tm_borders(alpha = 0.4, lwd = 10, col = "blue") +
-  tm_layout(legend.show = TRUE, scale = 0.3)
+  tm_layout(legend.show = TRUE, scale = 1)
 m
-tmap_save(m, f, width = 1000, height = 400)
-f = "figures/04-congruence.png"
-i = magick::image_read(f)
-i_trimmed = magick::image_trim(i)
-i_trimmed_border = magick::image_border(i_trimmed, color = "white", geometry = "10x10")
-i_trimmed_border
-magick::image_write(i_trimmed_border, f)
-detach("package:tmap", unload = TRUE)
+# tmap_save(m, f, width = 1000, height = 400)
+# f = "figures/04-congruence.png"
+# i = magick::image_read(f)
+# i_trimmed = magick::image_trim(i)
+# i_trimmed_border = magick::image_border(i_trimmed, color = "white", geometry = "10x10")
+# i_trimmed_border
+# magick::image_write(i_trimmed_border, f)
+# detach("package:tmap", unload = TRUE)
 # test visuals ----
 # tmap_mode("view")
 # qtm(aggzones, borders = "black") +
