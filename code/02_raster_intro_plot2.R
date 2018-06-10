e6d3920ae313b3d444c852f41fd681b939e67905
jn <tupiszakaczki@gmail.com>
2018-06-10 11:38:50 -0400

    raster-intro-plot2 tmap plot imprv

---
+++ code/02_raster_intro_plot2.R
@@ -0,0 +1,21 @@
+# second intro plot -----------------------------------------------------------
+library(tmap)
+library(rcartocolor)
+library(spDataLarge)
+
+# data read ---------------------------------------------------------------
+cla_raster = raster(system.file("raster/srtm.tif", package = "spDataLarge"))
+cat_raster = nlcd
+
+# colors create -----------------------------------------------------------
+terrain_colors = carto_pal(7, "TealRose")
+landcover_cols = c("#476ba0", "#aa0000", "#b2ada3", "#68aa63", "#a58c30", "#c9c977", "#dbd83d", "#bad8ea")
+
+# plots create ------------------------------------------------------------
+rast_srtm = tm_shape(cla_raster) + tm_raster(palette = terrain_colors, title = "Elevation (m)", style = "cont") + 
+  tm_layout(title = "A. Continuous data", legend.frame = TRUE, legend.position = c("right", "bottom"))
+
+rast_nlcd = tm_shape(cat_raster) + tm_raster(palette = landcover_cols, style = "cat", title = "Land cover") + 
+  tm_layout(title = "B. Categorical data", legend.frame = TRUE, legend.position = c("right", "bottom"))
+
+print(tmap_arrange(rast_srtm, rast_nlcd, ncol = 2))
