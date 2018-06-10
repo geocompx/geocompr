379f3a5503d5d05bb7c7df5bc83329e9e356ae30
jn <tupiszakaczki@gmail.com>
2018-06-09 21:22:09 -0400

    f5.21 tmap plot imprv

--- code/05-vector-rasterization1.R
+++ code/05-vector-rasterization1.R
@@ -1,13 +1,12 @@
 library(tmap)
-library(grid)
 
 r0p = tm_shape(cycle_hire_osm_projected) + tm_symbols(col = "capacity", title.col = "Capacity: ", size = 0.25) + 
-  tm_layout(title = "A. Points", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
+  tm_layout(title = "A. Points", legend.position = c("right", "bottom"))
 
-r1p = tm_shape(ch_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "B. Presence/absensce", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
+r1p = tm_shape(ch_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "B. Presence/absensce", legend.position = c("right", "bottom"))
 
-r2p = tm_shape(ch_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "C. Count", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
+r2p = tm_shape(ch_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "C. Count", legend.position = c("right", "bottom"))
 
-r3p = tm_shape(ch_raster3) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "D. Aggregated capacity", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
+r3p = tm_shape(ch_raster3) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "D. Aggregated capacity", legend.position = c("right", "bottom"))
 
 tmap_arrange(r0p, r1p, r2p, r3p, ncol = 2)
--- code/05-vector-rasterization2.R
+++ code/05-vector-rasterization2.R
@@ -1,32 +1,15 @@
 library(tmap)
 library(grid)
-
 california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
 
-r1po = tm_shape(california_raster1) +
-  tm_raster(legend.show = TRUE, title = "Values: ") +
-  tm_shape(california_raster_centr) + 
-  tm_dots() + 
-  tm_shape(california) +
-  tm_borders() + 
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4),
-            legend.position = c("right", "top"))
+r1po = tm_shape(california_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) + tm_dots() + 
+  tm_shape(california) + tm_borders() + 
+  tm_layout(title = "A. Line rasterization", legend.show = FALSE)
 
-r2po = tm_shape(california_raster2) +
-  tm_raster(legend.show = TRUE, title = "Values: ") +
-  tm_shape(california_raster_centr) + 
-  tm_dots() + 
-  tm_shape(california) +
-  tm_borders() + 
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4),
-            legend.position = c("right", "top"))
+r2po = tm_shape(california_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) + tm_dots() + 
+  tm_shape(california) + tm_borders() + 
+  tm_layout(title = "B. Polygon rasterization", legend.show = FALSE)
 
-grid.newpage()
-pushViewport(viewport(layout = grid.layout(2, 2, heights = unit(c(0.5, 5), "null"))))
-
-grid.text("A. Line rasterization", vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-print(r1po, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
-grid.text("B. Polygon rasterization", vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
-print(r2po, vp = viewport(layout.pos.row = 2, layout.pos.col = 2))
+tmap_arrange(r1po, r2po, ncol = 2)
