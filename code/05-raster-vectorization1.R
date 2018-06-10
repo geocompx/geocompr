f44df13fb491a08c20a082d2ed456b5a33050412
jn <tupiszakaczki@gmail.com>
2018-06-09 21:01:22 -0400

    f22 tmap plot imprv

--- code/05-raster-vectorization1.R
+++ code/05-raster-vectorization1.R
@@ -1,20 +1,10 @@
 library(tmap)
-library(grid)
 colfunc = colorRampPalette(c("lightyellow", "rosybrown"))
 
-p1 = tm_shape(elev) + 
-  tm_raster(legend.show = FALSE, palette = colfunc(36), n = 36) +
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4))
+p1 = tm_shape(elev) + tm_raster(legend.show = FALSE, palette = colfunc(36), n = 36) +
+  tm_layout(title = "Raster", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4))
 
-p2 = tm_shape(elev_point) + 
-  tm_bubbles(col = "layer", legend.col.show = FALSE, palette = colfunc(36), n = 36) +
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0.05, 4))
+p2 = tm_shape(elev_point) + tm_bubbles(col = "layer", legend.col.show = FALSE, palette = colfunc(36), n = 36) +
+  tm_layout(title = "Points", outer.margins = rep(0.01, 4), inner.margins = rep(0.05, 4))
 
-grid.newpage()
-pushViewport(viewport(layout = grid.layout(2, 2, heights = unit(c(0.25, 5), "null"))))
-grid.text("Raster", vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-grid.text("Points", vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
-print(p1, vp = viewport(layout.pos.col = 1))
-print(p2, vp = viewport(layout.pos.col = 2))
+tmap_arrange(p1, p2, ncol = 2)
