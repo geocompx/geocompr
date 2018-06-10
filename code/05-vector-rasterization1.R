846e0879cf12370b214e39bb150b57053908b821
jn <tupiszakaczki@gmail.com>
2018-06-09 21:06:27 -0400

    f20 tmap plot imprv

--- code/05-vector-rasterization1.R
+++ code/05-vector-rasterization1.R
@@ -1,38 +1,13 @@
 library(tmap)
 library(grid)
 
-r0p = tm_shape(cycle_hire_osm_projected) +
-  tm_bubbles(col = "capacity", title.col = "Capacity: ") + 
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4)) +
-  tm_layout(legend.position = c("right", "bottom"))
+r0p = tm_shape(cycle_hire_osm_projected) + tm_symbols(col = "capacity", title.col = "Capacity: ", size = 0.25) + 
+  tm_layout(title = "A. Points", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
 
-r1p = tm_shape(ch_raster1) + 
-  tm_raster(legend.show = TRUE, title = "Values: ") +
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4)) +
-  tm_layout(legend.position = c("right", "bottom"))
+r1p = tm_shape(ch_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "B. Presence/absensce", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
 
-r2p = tm_shape(ch_raster2) + 
-  tm_raster(legend.show = TRUE, title = "Values: ") +
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4)) +
-  tm_layout(legend.position = c("right", "bottom"))
+r2p = tm_shape(ch_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "C. Count", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
 
-r3p = tm_shape(ch_raster3) + 
-  tm_raster(legend.show = TRUE, title = "Values: ") +
-  tm_layout(outer.margins = rep(0.01, 4), 
-            inner.margins = rep(0, 4)) +
-  tm_layout(legend.position = c("right", "bottom"))
+r3p = tm_shape(ch_raster3) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(title = "D. Aggregated capacity", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4), legend.position = c("right", "bottom"))
 
-grid.newpage()
-pushViewport(viewport(layout = grid.layout(4, 2, heights = unit(rep(c(0.5, 5), 2), "null"))))
-
-grid.text("A. Points", vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-print(r0p, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
-grid.text("B. Presence/absensce", vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
-print(r1p, vp = viewport(layout.pos.row = 2, layout.pos.col = 2))
-grid.text("C. Count", vp = viewport(layout.pos.row = 3, layout.pos.col = 1))
-print(r2p, vp = viewport(layout.pos.row = 4, layout.pos.col = 1))
-grid.text("D. Aggregated capacity", vp = viewport(layout.pos.row = 3, layout.pos.col = 2))
-print(r3p, vp = viewport(layout.pos.row = 4, layout.pos.col = 2))
+tmap_arrange(r0p, r1p, r2p, r3p, ncol = 2)
