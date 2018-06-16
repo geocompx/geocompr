47cfb8b881dc8e3823af55c7f6840f9cccb620ce
jn <tupiszakaczki@gmail.com>
2018-06-16 12:45:28 -0400

    new raster tmap impl

--- code/05-vector-rasterization2.R
+++ code/05-vector-rasterization2.R
@@ -2,12 +2,12 @@ library(tmap)
 
 california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
 
-r1po = tm_shape(california_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") +
+r1po = tm_shape(california_raster1) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
   tm_layout(title = "A. Line rasterization", legend.show = FALSE)
 
-r2po = tm_shape(california_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") +
+r2po = tm_shape(california_raster2) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
   tm_layout(title = "B. Polygon rasterization", legend.show = FALSE)
