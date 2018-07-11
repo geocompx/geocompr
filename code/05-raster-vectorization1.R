e459e318c7693e86dcb22ae7c74e2be7187de2ff
jn <tupiszakaczki@gmail.com>
2018-07-10 21:02:16 -0400

    figures improvement vol1

--- code/05-raster-vectorization1.R
+++ code/05-raster-vectorization1.R
@@ -6,9 +6,9 @@ elev_point = rasterToPoints(elev, spatial = TRUE) %>% st_as_sf()
 colfunc = colorRampPalette(c("lightyellow", "rosybrown"))
 
 p1 = tm_shape(elev) + tm_raster(legend.show = FALSE, palette = colfunc(36), n = 36) +
-  tm_layout(title = "Raster", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4))
+  tm_layout(main.title = "Raster", outer.margins = rep(0.01, 4), inner.margins = rep(0, 4))
 
 p2 = tm_shape(elev_point) + tm_bubbles(col = "layer", legend.col.show = FALSE, palette = colfunc(36), n = 36) +
-  tm_layout(title = "Points", outer.margins = rep(0.01, 4), inner.margins = rep(0.05, 4))
+  tm_layout(main.title = "Points", outer.margins = rep(0.01, 4), inner.margins = rep(0.05, 4))
 
 tmap_arrange(p1, p2, ncol = 2)
--- code/05-vector-rasterization2.R
+++ code/05-vector-rasterization2.R
@@ -5,11 +5,11 @@ california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
 r1po = tm_shape(california_raster1) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
-  tm_layout(title = "A. Line rasterization", legend.show = FALSE)
+  tm_layout(main.title = "A. Line rasterization", main.title.size = 1, legend.show = FALSE)
 
 r2po = tm_shape(california_raster2) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
-  tm_layout(title = "B. Polygon rasterization", legend.show = FALSE)
+  tm_layout(main.title = "B. Polygon rasterization", main.title.size = 1, legend.show = FALSE)
 
 tmap_arrange(r1po, r2po, ncol = 2)
