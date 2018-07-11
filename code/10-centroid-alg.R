78380bb86fc4d29bfef3ebafeea85a67fd7cf745
Robin Lovelace <rob00x@gmail.com>
2018-07-11 01:02:33 +0100

    Update algorithm section code + prose
    - T_all is better name
    - Better verification of area

--- code/10-centroid-alg.R
+++ code/10-centroid-alg.R
@@ -1,15 +1,15 @@
 # Aim take a matrix representing a convex polygon, return its centroid
 # demonstrate how algorithms work
 i = 2:(nrow(poly_mat) - 2)
-Ti = lapply(i, function(x) {
+T_all = lapply(i, function(x) {
   rbind(O, poly_mat[x:(x + 1), ], O)
 })
-A = sapply(Ti, function(x) {
+A = sapply(T_all, function(x) {
   abs(x[1, 1] * (x[2, 2] - x[3, 2]) +
         x[2, 1] * (x[3, 2] - x[1, 2]) +
         x[3, 1] * (x[1, 2] - x[2, 2]) ) / 2
 })
-C = t(sapply(Ti,  function(x) (x[1, ] + x[2, ] + x[3, ]) / 3))
+C = t(sapply(T_all,  function(x) (x[1, ] + x[2, ] + x[3, ]) / 3))
 print(paste0("The area is: ", sum(A)))
 print(paste0(
   "The coordinates are: ",
