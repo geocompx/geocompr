c2c62d0fa572815b4bba9a37d7f3ad9a1639b7c4
Robin Lovelace <rob00x@gmail.com>
2018-07-25 11:04:50 +0100

    Update fig 4.6 - smaller

--- code/04-areal-example.R
+++ code/04-areal-example.R
@@ -7,9 +7,15 @@ rx = rbind(congruent, incongruent)
 m = tm_shape(rx) +
   tm_fill("value", breaks = seq(4, 6, by = 0.5)) +
   tm_borders(lwd = 2, col = "black", lty = 1) +
-  tm_facets(by = "level", drop.units = TRUE, ncol = 1) +
+  tm_facets(by = "level", drop.units = TRUE, ncol = 2) +
   tm_shape(aggregating_zones) +
-  tm_borders(alpha = 0.4, lwd = 10, col = "blue") +
-  tm_layout(legend.show = TRUE, scale = 1)
+  tm_borders(alpha = 0.4, lwd = 10, col = "blue")
 
-m
+m1 = m +
+  tm_layout(legend.show = FALSE, scale = 1)
+
+m2 = tm_shape(rx) +
+  tm_fill("value", breaks = seq(4, 6, by = 0.5)) +
+  tm_layout(legend.only = TRUE)
+
+tmap_arrange(m1, m2, ncol = 2)
