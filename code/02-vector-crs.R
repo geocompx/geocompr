3e68cc7d97a425ee55db4622e44aec4c1b5379fa
jn <tupiszakaczki@gmail.com>
2018-08-29 13:41:29 +0200

    unified crs figs

--- code/02-vector-crs.R
+++ code/02-vector-crs.R
@@ -13,7 +13,7 @@ p1 = ggplot() +
   geom_sf(data = new_vector2) + 
   coord_sf(datum = sf::st_crs(4326)) +
   labs(x = "Longitude", y = "Latitude") + 
-  scale_y_continuous(breaks = c(37.15, 37.3, 37.45)) +
+  scale_y_continuous(breaks = c(37.2, 37.3, 37.4, 37.5)) +
   scale_x_continuous(breaks = c(-113.2, -113.1, -113.0, -112.9)) + 
   theme(panel.grid = element_line(size = 1, color = "black", linetype = 2))
 
@@ -22,7 +22,7 @@ p2 = ggplot() +
   coord_sf(datum = sf::st_crs("+proj=utm +zone=12 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")) +
   labs(x = "x", y = "y") + 
   scale_y_continuous(breaks = c(4120000, 4130000, 4140000, 4150000)) +
-  scale_x_continuous(breaks = c(305000, 315000, 325000, 335000)) + 
+  scale_x_continuous(breaks = c(310000, 320000, 330000)) + 
   theme(panel.grid = element_line(size = 1, color = "black", linetype = 2))
 
 grid.arrange(p1, p2, nrow = 1)
