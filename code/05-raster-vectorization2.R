792a8ddd7c02edd55cd59681a5613115d312698c
jn <tupiszakaczki@gmail.com>
2018-08-29 13:52:20 +0200

    ras-vec figure small imprv

--- code/05-raster-vectorization2.R
+++ code/05-raster-vectorization2.R
@@ -5,12 +5,12 @@ tmap_options(main.title.size = 1)
 cols = c("clay" = "brown", "sand" = "rosybrown", "silt" = "sandybrown")
 
 p1p = tm_shape(grain) + tm_raster(legend.show = FALSE, palette = cols) +
-  tm_layout(main.title = "Raster")
+  tm_layout(main.title = "Raster", frame = FALSE)
 
 p2p = tm_shape(grain_poly) + tm_polygons("layer", legend.show = FALSE, palette = cols, lwd = 3) +
-  tm_layout(main.title = "Polygons")
+  tm_layout(main.title = "Polygons", frame = FALSE)
 
 p3p = tm_shape(grain_poly2) + tm_polygons("layer", legend.show = FALSE, palette = cols, lwd = 3) +
-  tm_layout(main.title = "Aggregated polygons")
+  tm_layout(main.title = "Aggregated polygons", frame = FALSE)
 
 tmap_arrange(p1p, p2p, p3p, ncol = 3)
