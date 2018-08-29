10ca6f6526406bb93e6ef22d22d8d1a43ad4676e
jn <tupiszakaczki@gmail.com>
2018-08-29 12:02:59 +0200

    vector crs code extracted

---
+++ code/02-vector-crs.R
@@ -0,0 +1,22 @@
+library(spDataLarge)
+library(tidyverse)
+library(sf)
+library(gridExtra)
+theme_set(theme_bw())
+
+## vector plot ---------------------------------------------------------------
+vector_filepath = system.file("vector/zion.gpkg", package = "spDataLarge")
+new_vector = st_read(vector_filepath)
+new_vector2 = st_transform(new_vector, 4326)
+
+p1 = ggplot() + 
+  geom_sf(data = new_vector2) + 
+  coord_sf(datum = sf::st_crs(4326)) +
+  # theme(axis.text = element_text(size = 4)) +
+  labs(x = "Longitude", y = "Latitude")
+
+p2 = ggplot() +
+  geom_sf(data = new_vector) +
+  coord_sf(datum = sf::st_crs("+proj=utm +zone=12 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")) +
+  # theme(axis.text = element_text(size = 4)) +
+  labs(x = "x", y = "y")
