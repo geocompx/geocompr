dd0ab27f10136fcf94839c15e8495fc87f6567aa
jn <tupiszakaczki@gmail.com>
2018-08-29 12:59:47 +0200

    vector crs code major improvement

--- code/02-vector-crs.R
+++ code/02-vector-crs.R
@@ -12,13 +12,17 @@ new_vector2 = st_transform(new_vector, 4326)
 p1 = ggplot() + 
   geom_sf(data = new_vector2) + 
   coord_sf(datum = sf::st_crs(4326)) +
-  # theme(axis.text = element_text(size = 4)) +
-  labs(x = "Longitude", y = "Latitude")
+  labs(x = "Longitude", y = "Latitude") + 
+  scale_y_continuous(breaks = c(37.15, 37.3, 37.45)) +
+  scale_x_continuous(breaks = c(-113.2, -113.1, -113.0, -112.9)) + 
+  theme(panel.grid = element_line(size = 1, color = "black", linetype = 2))
 
 p2 = ggplot() +
   geom_sf(data = new_vector) +
   coord_sf(datum = sf::st_crs("+proj=utm +zone=12 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")) +
-  # theme(axis.text = element_text(size = 4)) +
-  labs(x = "x", y = "y")
+  labs(x = "x", y = "y") + 
+  scale_y_continuous(breaks = c(4120000, 4130000, 4140000, 4150000)) +
+  scale_x_continuous(breaks = c(305000, 315000, 325000, 335000)) + 
+  theme(panel.grid = element_line(size = 1, color = "black", linetype = 2))
 
 grid.arrange(p1, p2, nrow = 1)
