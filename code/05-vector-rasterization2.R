a1e83ffd9d061f4946d92e87523374491b9e37b7
Nowosad <tupiszakaczki@gmail.com>
2020-03-25 20:10:02 +0100

    fixes new tmap

--- code/05-vector-rasterization2.R
+++ code/05-vector-rasterization2.R
@@ -17,12 +17,12 @@ if(!exists("raster_template2")) {
 
 california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
 
-r1po = tm_shape(california_raster1) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
+r1po = tm_shape(california_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
   tm_layout(main.title = "A. Line rasterization", main.title.size = 1, legend.show = FALSE)
 
-r2po = tm_shape(california_raster2) + tm_raster(col = "NAME", legend.show = TRUE, title = "Values: ") +
+r2po = tm_shape(california_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") +
   tm_shape(california_raster_centr) + tm_dots() + 
   tm_shape(california) + tm_borders() + 
   tm_layout(main.title = "B. Polygon rasterization", main.title.size = 1, legend.show = FALSE)
