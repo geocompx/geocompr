6e7907d9ad502a3e1bda0d1ef25e1040125249ff
jannes-m <malnamalja@gmx.de>
2021-08-03 23:43:24 +0200

    add svm autotuner and mlr3 advantages

--- code/11-spatial_cv_mlr3.R
+++ code/11-spatial_cv_mlr3.R
@@ -7,6 +7,9 @@ library("mlr3verse")
 
 # use 11-spatial-cv-jm.R as a template
 # replace RQGIS code by qgisprocess code
+# use benchmark_grid for comparing nsp with sp cv
+# be more consistent with the naming, spatial CV is an umbrella term for many different strategies, here, we are just using the k-means approach
+# in the conclusions say that there are other spatial CV methods available -> cross-reference Patrick's upcoming paper
 
 # just use lsl and ta from spDataLarge, do no longer refer to RSAGA since it is
 # deprecated and it is also not that interesting for the reader to construct the
@@ -14,6 +17,11 @@ library("mlr3verse")
 data("lsl", package = "spDataLarge")
 data("ta", package = "spDataLarge")
 # data("landslides", package = "RSAGA")
+
+#**********************************************************
+# spatial cv with glm----
+#**********************************************************
+
 # create 'sf' object
 data_sf = sf::st_as_sf(lsl, coords = c("x", "y"), crs = 32717)
 tab = table(data_sf$lslpts)
@@ -63,3 +71,82 @@ rr = resample(task = task,
               resampling = perf_level,
               store_models = TRUE)
 rr$aggregate(measures = msr("classif.auc"))
+
+#**********************************************************
+# hyperparameter tuning ksvm----
+#**********************************************************
+
+# filter(lrns, grepl("svm", class)) %>% 
+#   dplyr::select(class, name, short.name, package)
+install.packages("kernlab")
+remotes::install_github("mlr-org/mlr3extralearners")
+# make many more learners available
+library("mlr3extralearners")
+mlr_learners |>
+  as.data.table() |>
+  dplyr::filter(grepl("svm", key))
+
+lrn_ksvm = lrn("classif.ksvm", predict_type = "prob", kernel = "rbfdot")
+lrn_ksvm$help()
+# available hyperparameters
+lrn_ksvm$param_set
+
+# define the search space
+# ps = ParamSet$new(
+#   params = list(
+#     # if you use hyperpars from different "instances", here from the filter
+#     # and from the learner, you have to use the . notation to refer to a
+#     # specific hyperparameter, solution found here:
+#     # https://mlr3book.mlr-org.com/pipe-modeling.html
+#     ParamDbl$new(id = "classif.ksvm.C", lower = -12, upper = 15),
+#     ParamDbl$new(id = "classif.ksvm.sigma", lower = -15, upper = 6)
+#   )
+# )
+
+# define the outer limits of the randomly selected hyperparameters
+ps = ps(
+  C = p_dbl(lower = -12, upper = 15, trafo = function(x) 2^x),
+  sigma = p_dbl(lower = -15, upper = 6, trafo = function(x) 2^x)
+)
+
+# resampling_coords = rsmp("spcv_coords", folds = 5)$instantiate(task)
+# # install.packages(c("patchwork", "ggtext"))
+# autoplot(resampling_coords,
+#          size = 1.5, fold_id = 1, task = task) +
+#   ggplot2::scale_y_continuous(breaks = seq(-3.97, -4, -0.01)) +
+#   ggplot2::scale_x_continuous(breaks = seq(-79.06, -79.08, -0.01))
+
+# nested resampling
+at_ksvm = AutoTuner$new(
+  learner = lrn_ksvm,
+  # five spatially disjoint partitions (inner resampling, tuning level)
+  resampling = rsmp("spcv_coords", folds = 5),
+  measure = msr("classif.auc"),
+  search_space = ps,
+  # use 50 randomly selected hyperparameters
+  terminator = trm("evals", n_evals = 50),
+  tuner = tnr("random_search")
+)
+
+# reduce verbosity
+lgr::get_logger("mlr3")$set_threshold("warn")
+# future::plan("multiprocess", workers = 4)
+# this will fit 125,500 models, so this might take a while
+progressr::with_progress(expr = {
+  rr = resample(task = task,
+                learner = at_ksvm,
+                # outer resampling (performance level)
+                resampling = rsmp("repeated_spcv_coords", folds = 5,
+                                  repeats = 100),
+                store_models = TRUE)
+})
+
+# Advantages mlr3
+# - create benchmark grid, i.e., combine all tasks, learners and resampling
+# strategies which you would like to compare
+# - pipelines (data transformations)
+# - fallback learners -> making sure that process is not stopped just because of one failed model, etc.)
+# - feature selection
+# - filtering
+# - autoplot
+# - ...
