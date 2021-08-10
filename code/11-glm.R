a83cf3b518e33c273cf4a6089cf6b84a2317094b
jannes-m <malnamalja@gmx.de>
2021-08-11 01:58:28 +0200

    replace mlr by mlr3 output

---
+++ code/11-glm.R
@@ -0,0 +1,187 @@
+# Filename: 11-glm.R (2021-09-08)
+#
+# TO DO: Introducing spatial cross-validation with the help of mlr3
+#
+# Author(s): Jannes Muenchow
+#
+#**********************************************************
+# contents----
+#**********************************************************
+#
+# 1 attach packages and data
+# 2 data preprocessing
+# 3 terrain attributes
+# 4 modeling 
+# 5 spatial prediction
+#
+#**********************************************************
+# 1 attach packages and data----
+#**********************************************************
+
+# attach packages
+library(dplyr)
+library(ggplot2)
+library(mlr3)
+library(mlr3extralearners)
+library(mlr3learners)
+library(mlr3spatiotempcv)
+library(mlr3tuning)
+library(raster)
+library(sf)
+library(tmap)
+
+#**********************************************************
+# 2 modeling----
+#**********************************************************
+
+# attach data
+data("lsl", "ta", package = "spDataLarge")
+
+# 2.1 create a mlr3 task====
+#**********************************************************
+# id = name of the task
+# target = response variable
+# spatial: setting up the scene for spatial cv (hence x and y will not be used
+# as predictors but as coordinates for kmeans clustering)
+# all variables in data will be used in the model
+
+task = TaskClassifST$new(
+  id = "lsl_glm_sp",
+  backend = mlr3::as_data_backend(lsl), target = "lslpts", positive = "TRUE",
+  extra_args = list(
+    coordinate_names = c("x", "y"),
+    coords_as_features = FALSE,
+    crs = 32717)
+)
+# pls note that you can use a task created by TaskClassifST for both the spatial
+# and the conventiona CV approach
+
+# 2.2 construct a learner and run the model====
+#**********************************************************
+learner = lrn("classif.log_reg", predict_type = "prob")
+
+# 2.3 cross-validation====
+#**********************************************************
+
+# specify the reampling method, i.e. spatial CV with 100 repetitions and 5 folds
+# -> in each repetition dataset will be splitted into five folds
+# method: repeated_spcv_coords -> spatial partioning
+# method: repeated_cv -> non-spatial partitioning
+rsmp_sp = rsmp("repeated_spcv_coords", folds = 5, repeats = 100)
+rsmp_nsp = rsmp("repeated_cv", folds = 5, repeats = 100)
+
+# why do we need the seed?
+# the seed is needed when to make sure
+# that always the same spatial partitions are used when re-running the code
+set.seed(012348)  
+bm = benchmark_grid(tasks = list(task_sp, task_nsp), 
+                    learners = learner,
+                    resamplings = list(rsmp_sp, rsmp_nsp))
+
+# create your design
+grid = benchmark_grid(tasks = task_sp, 
+                      learners = learner,
+                      resamplings = list(rsmp_sp, rsmp_nsp))
+print(grid)
+# why do we need the seed?
+# the seed is needed when to make sure
+# that always the same spatial partitions are used when re-running the code
+set.seed(012348)
+# run the cross-validations (spatial and conventional)
+bmr = benchmark(grid, store_backends = FALSE)
+
+# plot your result
+# library(mlr3viz)
+# p1 = autoplot(bmr, measure = msr("classif.auc"))
+# p1$labels$y = "AUROC"
+# p1$layers[[1]]$aes_params$fill = c("lightblue2", "mistyrose2")
+# p1 + 
+#   scale_x_discrete(labels=c("spatial CV", "conventional CV")) +
+#   ggplot2::theme_bw()
+
+# instead of using autoplot, it might be easier to create the figure yourself
+agg = bmr$aggregate(measures = msr("classif.auc"))
+# extract the AUROC values and put them into one data.table
+d = purrr::map_dfr(agg$resample_result, ~ .$score(msr("classif.auc")))
+# rename the levels of resampling_id
+d[, resampling_id := as.factor(resampling_id) |>
+    forcats::fct_recode("conventional CV" = "repeated_cv", 
+                        "spatial CV" = "repeated_spcv_coords") |> 
+    forcats::fct_rev()]
+# create the boxplot which shows the overfitting in the nsp-case
+ggplot2::ggplot(data = d, mapping = aes(x = resampling_id, y = classif.auc)) +
+  geom_boxplot(fill = c("lightblue2", "mistyrose2")) +
+  theme_bw() +
+  labs(y = "AUROC", x = "")
+
+# save your result
+saveRDS(agg, file = "extdata/11-sp-conv-cv.rds")
+# saveRDS(sp_cv, file = "extdata/sp_cv.rds")
+# saveRDS(conv_cv, file = "extdata/conv_cv.rds")
+
+#**********************************************************
+# 3 spatial prediction----
+#**********************************************************
+
+#**********************************************************
+# 3.1 make the prediction====
+#**********************************************************
+
+learner$train(task)
+fit = learner$model
+# according to learner$help() the default for predictions was adjusted to FALSE,
+# since we would like to have TRUE predictions, we have to change back
+fit$coefficients = fit$coefficients * -1
+
+pred = raster::predict(object = ta, model = fit, fun = predict,
+                       type = "response")
+
+# make the prediction "manually"
+ta_2 = stack(ta)
+newdata = as.data.frame(as.matrix(ta_2))
+colSums(is.na(newdata))  # ok, there are NAs
+ind = rowSums(is.na(newdata)) == 0
+tmp = learner$predict_newdata(newdata = newdata[ind, ], task = task)
+newdata[ind, "pred"] = as.data.table(tmp)[["prob.TRUE"]]
+# check
+all.equal(values(pred), newdata$pred)
+pred_2 = ta$slope
+values(pred_2) = newdata$pred
+all.equal(pred, pred_2)
+plot(stack(pred, pred_2))
+
+#**********************************************************
+# 3.2 plot the prediction====
+#**********************************************************
+
+hs = hillShade(ta$slope * pi / 180, 
+               raster::terrain(ta$elev, opt = "aspect", unit = "radians"),
+               40, 270)
+plot(hs, col = gray(seq(0, 1, length.out = 100)), legend = FALSE)
+# plot(pred, col = RColorBrewer::brewer.pal("YlOrRd", n =  9), add = TRUE, 
+#      alpha = 0.6)
+plot(pred, col = RColorBrewer::brewer.pal(name = "Reds", 9), add = TRUE, 
+     alpha = 0.6)
+
+# or using tmap 
+# white raster to only plot the axis ticks, otherwise gridlines would be visible
+lsl_sf = st_as_sf(lsl, coords = c("x", "y"), crs = 32717)
+rect = tmaptools::bb_poly(hs)
+bbx = tmaptools::bb(hs, xlim = c(-0.02, 1), ylim = c(-0.02, 1), relative = TRUE)
+tm_shape(hs, bbox = bbx) +
+  tm_grid(col = "black", n.x = 1, n.y = 1, labels.inside.frame = FALSE,
+          labels.rot = c(0, 90)) +
+  tm_raster(palette = "white", legend.show = FALSE) +
+  # hillshade
+  tm_shape(mask(hs, study_mask), bbox = bbx) +
+  tm_raster(palette = gray(0:100 / 100), n = 100,
+            legend.show = FALSE) +
+  # prediction raster
+  tm_shape(mask(pred,study_mask)) +
+  tm_raster(alpha = 0.5, palette = "Reds", n = 6, legend.show = TRUE,
+            title = "Susceptibility") +
+  # rectangle and outer margins
+  qtm(rect, fill = NULL) +
+  tm_layout(outer.margins = c(0.04, 0.04, 0.02, 0.02), frame = FALSE,
+            legend.position = c("left", "bottom"),
+            legend.title.size = 0.9)
