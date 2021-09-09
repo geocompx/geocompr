ba95fcad3069621a226e87a88ad40492f879cdff
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-09-09 18:05:36 +0200

    moves raster-intro-plot to terra

--- code/02-raster-intro-plot2.R
+++ code/02-raster-intro-plot2.R
@@ -2,24 +2,25 @@
 library(tmap)
 library(rcartocolor)
 library(spDataLarge)
-library(raster)
+library(terra)
 
 # data read ---------------------------------------------------------------
-cla_raster = raster(system.file("raster/srtm.tif", package = "spDataLarge"))
-cat_raster = nlcd
-
-# colors create -----------------------------------------------------------
-terrain_colors = carto_pal(7, "TealRose")
-landcover_cols = c("#476ba0", "#aa0000", "#b2ada3", "#68aa63", "#a58c30", "#c9c977", "#dbd83d", "#bad8ea")
+cla_raster = rast(system.file("raster/srtm.tif", package = "spDataLarge"))
+cat_raster = rast(system.file("raster/nlcd.tif", package = "spDataLarge"))
 
 # plots create ------------------------------------------------------------
-rast_srtm = tm_shape(cla_raster) + tm_raster(palette = terrain_colors, title = "Elevation (m)", style = "cont") + 
-  tm_layout(main.title = "A. Continuous data", legend.frame = TRUE, legend.position = c("right", "bottom"))
+rast_srtm = tm_shape(cla_raster) +
+  tm_raster(palette = carto_pal(7, "Geyser"),
+            title = "Elevation (m)", style = "cont") + 
+  tm_layout(main.title = "A. Continuous data",
+            legend.frame = TRUE, 
+            legend.position = c("right", "bottom"))
 
-rast_nlcd = tm_shape(cat_raster) + tm_raster(palette = landcover_cols, style = "cat", title = "Land cover") + 
-  tm_layout(main.title = "B. Categorical data", legend.frame = TRUE, legend.position = c("right", "bottom"))
-# names(cat_raster)
-# names(cla_raster)
+rast_nlcd = tm_shape(cat_raster) +
+  tm_raster(drop.levels = TRUE, title = "Land cover") + 
+  tm_layout(main.title = "B. Categorical data",
+            legend.frame = TRUE, 
+            legend.position = c("right", "bottom"))
 
-tmap_arrange(rast_srtm, rast_nlcd, ncol = 2)
+tmap_arrange(rast_srtm, rast_nlcd, nrow = 1)
 
