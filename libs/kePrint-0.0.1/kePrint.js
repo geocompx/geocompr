3bcaa611ff9594f1f3680f7bc251343379df49e0
Nowosad <Nowosad@users.noreply.github.com>
2021-09-10 11:55:25 +0000

    Deploy commit: goes back to fALSE 6b53971e1959a97ae518bdd0057f2c938b755cae

--- libs/kePrint-0.0.1/kePrint.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    if (typeof $('[data-toggle="tooltip"]').tooltip === 'function') {
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-    if ($('[data-toggle="popover"]').popover === 'function') {
-        $('[data-toggle="popover"]').popover();
-    }
-});
