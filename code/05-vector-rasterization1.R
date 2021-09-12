8ec091d6dba7a8b1045c03d9c3cd602595ff802e
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-09-12 16:08:42 +0200

    updates vec-rast1 plot to terra

--- code/05-vector-rasterization1.R
+++ code/05-vector-rasterization1.R
@@ -1,29 +1,38 @@
 library(sf)
 library(tmap)
 library(spData)
-library(raster)
+library(terra)
 
 if(!exists("cycle_hire_osm_projected")) {
-  cycle_hire_osm_projected = st_transform(cycle_hire_osm, 27700)
-  raster_template = raster(extent(cycle_hire_osm_projected), resolution = 1000,
-                           crs = st_crs(cycle_hire_osm_projected)$proj4string)
+  cycle_hire_osm_projected = st_transform(cycle_hire_osm, "EPSG:27700")
+  raster_template = rast(ext(cycle_hire_osm_projected), resolution = 1000,
+                         crs = "EPSG:27700")
   
-  ch_raster1 = rasterize(cycle_hire_osm_projected, raster_template, field = 1)
+  ch_raster1 = rasterize(vect(cycle_hire_osm_projected), raster_template, field = 1)
   
-  ch_raster2 = rasterize(cycle_hire_osm_projected, raster_template, 
-                         field = 1, fun = "count")
+  ch_raster2 = rasterize(vect(cycle_hire_osm_projected), raster_template, 
+                         field = 1, fun = "length")
   
-  ch_raster3 = rasterize(cycle_hire_osm_projected, raster_template, 
+  ch_raster3 = rasterize(vect(cycle_hire_osm_projected), raster_template, 
                          field = "capacity", fun = sum)
 }
 
-r0p = tm_shape(cycle_hire_osm_projected) + tm_symbols(col = "capacity", title.col = "Capacity: ", size = 0.1) + 
+r0p = tm_shape(cycle_hire_osm_projected) + 
+  tm_symbols(col = "capacity", title.col = "Capacity: ", size = 0.1) + 
   tm_layout(main.title = "A. Points", main.title.size = 1, legend.position = c("right", "bottom"))
 
-r1p = tm_shape(ch_raster1) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(main.title = "B. Presence/absence", main.title.size = 1, legend.position = c("right", "bottom"))
+r1p = tm_shape(ch_raster1) + 
+  tm_raster(legend.show = TRUE, title = "Values: ") + 
+  tm_layout(main.title = "B. Presence/absence", main.title.size = 1, 
+            legend.position = c("right", "bottom"))
 
-r2p = tm_shape(ch_raster2) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(main.title = "C. Count", main.title.size = 1, legend.position = c("right", "bottom"))
+r2p = tm_shape(ch_raster2) + 
+  tm_raster(legend.show = TRUE, title = "Values: ") + 
+  tm_layout(main.title = "C. Count", main.title.size = 1, legend.position = c("right", "bottom"))
 
-r3p = tm_shape(ch_raster3) + tm_raster(legend.show = TRUE, title = "Values: ") + tm_layout(main.title = "D. Aggregated capacity", main.title.size = 1, legend.position = c("right", "bottom"))
+r3p = tm_shape(ch_raster3) + 
+  tm_raster(legend.show = TRUE, title = "Values: ") +
+  tm_layout(main.title = "D. Aggregated capacity", main.title.size = 1,
+            legend.position = c("right", "bottom"))
 
 tmap_arrange(r0p, r1p, r2p, r3p, ncol = 2)
