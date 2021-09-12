6d48a37014c6b4f39814773561fb32fa2245081b
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-09-12 16:08:50 +0200

    updates vec-rast2 plot to terra

--- code/05-vector-rasterization2.R
+++ code/05-vector-rasterization2.R
@@ -2,29 +2,36 @@ library(tmap)
 
 if(!exists("raster_template2")) {
   library(sf)
-  library(raster)
+  library(terra)
   library(spData)
   library(spDataLarge)
   california = dplyr::filter(us_states, NAME == "California")
   california_borders = st_cast(california, "MULTILINESTRING")
-  raster_template2 = raster(extent(california), resolution = 0.5,
-                            crs = st_crs(california)$proj4string)
+  raster_template2 = rast(ext(california), resolution = 0.5,
+                          crs = crs(california))
   
-  california_raster1 = rasterize(as(california_borders, "Spatial"), raster_template2)
-  
-  california_raster2 = rasterize(as(california, "Spatial"), raster_template2)
+  california_raster1 = rasterize(vect(california_borders), raster_template2,
+                                 touches = TRUE)
+  california_raster2 = rasterize(vect(california), raster_template2)
 }
 
-california_raster_centr = rasterToPoints(raster_template2, spatial = TRUE)
+california_raster_centr = st_as_sf(as.polygons(raster_template2))
+california_raster_centr = st_centroid(california_raster_centr)
 
-r1po = tm_shape(california_raster1) + tm_raster(col = "layer", legend.show = TRUE, title = "Values: ") +
-  tm_shape(california_raster_centr) + tm_dots() + 
+r1po = tm_shape(california_raster1) + 
+  tm_raster(col = "layer", legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) +
+  tm_dots() + 
   tm_shape(california) + tm_borders() + 
-  tm_layout(main.title = "A. Line rasterization", main.title.size = 1, legend.show = FALSE)
+  tm_layout(main.title = "A. Line rasterization", main.title.size = 1, 
+            legend.show = FALSE, frame = FALSE)
 
-r2po = tm_shape(california_raster2) + tm_raster(col = "layer", legend.show = TRUE, title = "Values: ") +
-  tm_shape(california_raster_centr) + tm_dots() + 
+r2po = tm_shape(california_raster2) +
+  tm_raster(col = "layer", legend.show = TRUE, title = "Values: ") +
+  tm_shape(california_raster_centr) + 
+  tm_dots() + 
   tm_shape(california) + tm_borders() + 
-  tm_layout(main.title = "B. Polygon rasterization", main.title.size = 1, legend.show = FALSE)
+  tm_layout(main.title = "B. Polygon rasterization", main.title.size = 1,
+            legend.show = FALSE, frame = FALSE)
 
 tmap_arrange(r1po, r2po, ncol = 2)
