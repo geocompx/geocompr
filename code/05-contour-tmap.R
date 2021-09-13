9ee5c25435d0d8c3bdb90fff91ecff7db9f273b0
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-09-13 17:56:34 +0200

    fixes contour map

--- code/05-contour-tmap.R
+++ code/05-contour-tmap.R
@@ -1,8 +1,10 @@
 library(tmap)
+library(sf)
 library(terra)
 dem = rast(system.file("raster/dem.tif", package = "spDataLarge"))
 # create hillshade
-hs = shade(slope = terrain(dem, "slope"), aspect = terrain(dem, "aspect"))
+hs = shade(slope = terrain(dem, "slope", unit = "radians"), 
+           aspect = terrain(dem, "aspect", unit = "radians"))
 # create contour
 cn = st_as_sf(as.contour(dem))
 
@@ -10,7 +12,7 @@ tm1 = tm_shape(hs) +
 	tm_grid(col = "black", n.x = 2, n.y = 2, labels.rot = c(0, 90)) +
 	tm_raster(palette = gray(0:100 / 100), n = 100, legend.show = FALSE) +
 	tm_shape(dem) +
-	tm_raster(alpha = 0.5, palette = hcl.colors(25, "Geyser"), legend.show = FALSE) +
+	tm_raster(alpha = 0.6, palette = hcl.colors(25, "Geyser"), legend.show = FALSE) +
 	tm_shape(cn) +
 	tm_iso("level", col = "white") +
 	tm_layout(outer.margins = c(0.04, 0.04, 0.02, 0.02), frame = FALSE)
