f27ec1ec726403cb1d8cbade67bd45860270ce92
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-10-05 09:26:38 +0200

    improves vect ras1

--- code/05-vector-rasterization1.R
+++ code/05-vector-rasterization1.R
@@ -19,20 +19,22 @@ if(!exists("cycle_hire_osm_projected")) {
 
 r0p = tm_shape(cycle_hire_osm_projected) + 
   tm_symbols(col = "capacity", title.col = "Capacity: ", size = 0.1) + 
-  tm_layout(main.title = "A. Points", main.title.size = 1, legend.position = c("right", "bottom"))
+  tm_layout(main.title = "A. Points", main.title.size = 1, 
+            legend.position = c("right", "bottom"), legend.frame = TRUE)
 
 r1p = tm_shape(ch_raster1) + 
   tm_raster(legend.show = TRUE, title = "Values: ") + 
   tm_layout(main.title = "B. Presence/absence", main.title.size = 1, 
-            legend.position = c("right", "bottom"))
+            legend.position = c("right", "bottom"), legend.frame = TRUE)
 
 r2p = tm_shape(ch_raster2) + 
   tm_raster(legend.show = TRUE, title = "Values: ") + 
-  tm_layout(main.title = "C. Count", main.title.size = 1, legend.position = c("right", "bottom"))
+  tm_layout(main.title = "C. Count", main.title.size = 1,
+            legend.position = c("right", "bottom"), legend.frame = TRUE)
 
 r3p = tm_shape(ch_raster3) + 
   tm_raster(legend.show = TRUE, title = "Values: ") +
   tm_layout(main.title = "D. Aggregated capacity", main.title.size = 1,
-            legend.position = c("right", "bottom"))
+            legend.position = c("right", "bottom"), legend.frame = TRUE)
 
 tmap_arrange(r0p, r1p, r2p, r3p, ncol = 2)
