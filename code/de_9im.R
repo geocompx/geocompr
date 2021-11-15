af95b09af66d4d57d2be1b499d304cb42c498fab
Robin Lovelace <rob00x@gmail.com>
2021-11-15 20:16:45 +0000

    MVP for de_9im fun

--- code/de_9im.R
+++ code/de_9im.R
@@ -7,12 +7,17 @@
 #' library(sf)
 #' x = st_sfc(st_polygon(list(rbind(c(0, 0), c(1, 0), c(1, 1), c(0, 0)))))
 #' y = st_sfc(st_polygon(list(rbind(c(0, 0), c(0, 1), c(1, 1), c(0, 0)))))
-#' de_9im
-de_9im = function(x, y, object_names = c("x", "y")) {
+#' de_9im(x, y)
+de_9im = function(x, y, object_names = c("x", "y"), funs = list("st_intersects", "st_disjoint"), sparse = FALSE) {
+  requireNamespace("sf", quietly = TRUE)
   if(is(x, "sfc") && is(y, "sfc")) {
     x = st_sf(data.frame(Object = object_names[1]), geometry = x)
     y = st_sf(data.frame(Object = object_names[2]), geometry = y)
   }
-  browser()
-  ps1 = rbind(psf1, psf2)
+  xy = rbind(x, y)
+  funs_matched = lapply(funs, match.fun)
+  res = lapply(seq(length(funs)), function(i) {
+    funs_matched[[i]](x, y, sparse = sparse)
+  })
+  unlist(res)
 }
