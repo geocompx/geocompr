887d73d37349d065d4b6b0744b53b008cd8e3ac5
Robin Lovelace <rob00x@gmail.com>
2021-12-09 23:20:04 +0000

    Global object rename w. fd:
    sd 'bb_world' 'bb' 01-introduction.Rmd
    02-spatial-data.Rmd
    03-attribute-operations.Rmd
    04-spatial-operations.Rmd
    05-geometry-operations.Rmd
    06-reproj.Rmd
    07-read-write-plot.Rmd
    08-mapping.Rmd
    09-gis.Rmd
    10-algorithms.Rmd
    11-spatial-cv.Rmd
    12-transport.Rmd
    13-location.Rmd
    14-eco.Rmd
    15-synthesis.Rmd
    DESCRIPTION
    README.Rmd
    README.md
    _01-ex.Rmd
    _02-ex.Rmd
    _03-ex.Rmd
    _04-ex.Rmd
    _05-ex.Rmd
    _06-ex.Rmd
    _07-ex.Rmd
    _404.Rmd
    apps/CycleHireApp/app.R
    apps/coffeeApp/app.R
    code/01-cranlogs.R
    code/02-contpop.R
    code/02-datum-fig.R
    code/02-raster-crs.R
    code/02-raster-intro-plot.R
    code/02-raster-intro-plot2.R
    code/02-vector-crs.R
    code/02-vectorplots.R
    code/03-cont-raster-plot.R
    code/04-areal-example.R
    code/04-focal-example.R
    code/04-local-operations.R
    code/04-ndvi.R
    code/04-raster-subset.R
    code/04-spatial-join.R
    code/05-bilinear.R
    code/05-contour-tmap.R
    code/05-extend-example.R
    code/05-pointextr.R
    code/05-raster-vectorization1.R
    code/05-raster-vectorization2.R
    code/05-us-regions.R
    code/05-vector-rasterization1.R
    code/05-vector-rasterization2.R
    code/05-venn-clip.R
    code/08-break-styles.R
    code/08-layout1.R
    code/08-layout2.R
    code/08-layout3.R
    code/08-map-pkgs.R
    code/08-tmpal.R
    code/08-tmshape.R
    code/08-tmstyles.R
    code/08-urban-animation.R
    code/08-usboundaries.R
    code/09-saga-wetness.R
    code/09-sliver.R
    code/09-tsp.R
    code/10-centroid-alg.R
    code/10-centroid-setup.R
    code/10-hello.R
    code/10-polycent.R
    code/11-partitioning.R
    code/11-spatial-cv-jm.R
    code/11-svm.R
    code/12-cycleways.R
    code/12-desire.R
    code/12-transport-data-gen.R
    code/12-zones.R
    code/13-location-jm.R
    code/14-eco.R
    code/14-rf.R
    code/add-impact.R
    code/before_script.R
    code/chapters/01-introduction.R
    code/chapters/02-spatial-data.R
    code/chapters/03-attribute-operations.R
    code/chapters/04-spatial-operations.R
    code/chapters/05-geometry-operations.R
    code/chapters/06-reproj.R
    code/chapters/07-read-write-plot.R
    code/chapters/08-mapping.R
    code/chapters/09-gis.R
    code/chapters/10-algorithms.R
    code/chapters/11-spatial-cv.R
    code/chapters/12-transport.R
    code/chapters/13-location.R
    code/chapters/14-eco.R
    code/chapters/15-synthesis.R
    code/chapters/README.R
    code/chapters/index.R
    code/chapters/references.R
    code/de_9im.R
    code/extra-pkgs.R
    code/frontcover.R
    code/generate-chapter-code.R
    code/hex_sticker.R
    code/list-contributors.R
    code/old-to-future-remove/04_spatial_incongruence.R
    code/old-to-future-remove/06_raster_reprojection_tests.R
    code/old-to-future-remove/08-uscolonize.R
    code/old-to-future-remove/10-centroid.R
    code/old-to-future-remove/10-earthquakes.R
    code/old-to-future-remove/12-code-extension.R
    code/old-to-future-remove/12-desire-front.R
    code/old-to-future-remove/globe.R
    code/old-to-future-remove/sfr-class-diagram-gen.R
    code/old-to-future-remove/spData.R
    code/sf-classes.R
    code/sf-revdep.R
    extdata/postgis_data.Rdata
    geocompr.Rproj
    index.Rmd
    references.Rmd

--- code/04-spatial-join.R
+++ code/04-spatial-join.R
@@ -10,10 +10,10 @@ library(tmap)
 
 if(!exists("random_joined")) {
   set.seed(2018)
-  bb_world = st_bbox(world)
+  bb = st_bbox(world)
   random_df = tibble::tibble(
-    x = runif(n = 10, min = bb_world[1], max = bb_world[3]),
-    y = runif(n = 10, min = bb_world[2], max = bb_world[4])
+    x = runif(n = 10, min = bb[1], max = bb[3]),
+    y = runif(n = 10, min = bb[2], max = bb[4])
   )
   random_points = st_as_sf(random_df, coords = c("x", "y")) %>% 
     st_set_crs(4326)
@@ -29,26 +29,26 @@ random_joined$name_long = as.character(random_joined$name_long)
 jm0 = tm_shape(world) + tm_borders(lwd = 0.2) + tm_format("World")
 
 jm1 = jm0 +
-  tm_shape(shp = random_points, bbox = bb_world) +
+  tm_shape(shp = random_points, bbox = bb) +
   tm_symbols(col = "black", shape = 4, border.lwd = 2) +
   tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm2 = jm0 +
-  tm_shape(world_random, bbox = bb_world) +
+  tm_shape(world_random, bbox = bb) +
   tm_fill(col = "name_long", palette = "Dark2") +
   tm_layout(legend.show = FALSE)
   #tm_borders(col = "name_long", lwd = 4) + # issue with tmap: no variable border
   # tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm3 = jm0 +
-  tm_shape(shp = random_joined, bbox = bb_world) +
+  tm_shape(shp = random_joined, bbox = bb) +
   tm_symbols(col = "name_long", shape = 4, border.lwd = 2, palette = "Dark2") +
   tm_layout(legend.show = FALSE)
 # +
 #   tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm4 = jm0 +
-  tm_shape(shp = random_joined, bbox = bb_world) +
+  tm_shape(shp = random_joined, bbox = bb) +
   tm_symbols(col = "name_long", shape = 4, border.lwd = 2, palette = "Dark2") +
   tm_layout(legend.only = TRUE)
 
