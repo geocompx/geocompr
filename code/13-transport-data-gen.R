f7470b9110b673ab7826a37b42bda6aaa1d64dfe
Robin Lovelace <rob00x@gmail.com>
2022-08-28 23:11:35 +0100

    Make data transport data gen script reproducible (#835)

--- code/13-transport-data-gen.R
+++ code/13-transport-data-gen.R
@@ -3,26 +3,34 @@
 # set-up ------------------------------------------------------------------
 
 # remotes::install_github("ropensci/osmdata")
+library(sf)
+sf::sf_use_s2(FALSE)
 library(osmdata)
+library(tidyverse)
+
 
 # get region data ---------------------------------------------------------
 
-region = getbb("Bristol", format_out = "sf_polygon") |>
+region = getbb("Bristol", format_out = "sf_polygon", limit = 1) |>
   st_set_crs("EPSG:4326")
 region = st_sf(data.frame(Name = "Bristol (OSM)"), geometry = region$geometry)
+mapview::mapview(region)
 saveRDS(region, "extdata/bristol-region.rds")
 remotes::install_github("robinlovelace/ukboundaries")
 library(ukboundaries)
 region_ttwa = ttwa_simple |> 
   filter(grepl("Bristol", ttwa11nm)) |> 
-  select(Name = ttwa11nm)
+  select(Name = ttwa11nm) |> 
+  sf::st_set_crs("EPSG:4326")
 region_ttwa$Name = "Bristol (TTWA)"
 
 # get zones ---------------------------------------------------------------
 
-zones_cents = st_centroid(msoa2011_vsimple)[region_ttwa, ]
+msoas = msoa2011_vsimple |> 
+  sf::st_set_crs("EPSG:4326")
+zones_cents = st_centroid(msoas)[region_ttwa, ]
 plot(zones_cents$geometry)
-zones = msoa2011_vsimple[msoa2011_vsimple$msoa11cd %in% zones_cents$msoa11cd, ] |> 
+zones = msoas[msoas$msoa11cd %in% zones_cents$msoa11cd, ] |> 
   select(geo_code = msoa11cd, name = msoa11nm) |> 
   mutate_at(1:2, as.character)
 plot(zones$geometry, add = TRUE)
@@ -30,15 +38,12 @@ plot(zones$geometry, add = TRUE)
 # get origin-destination data ---------------------------------------------
 
 # using wicid open data - see http://wicid.ukdataservice.ac.uk/
-unzip("~/Downloads/wu03ew_v2.zip")
-od_all = read_csv("wu03ew_v2.csv")
-file.remove("wu03ew_v2.csv")
+od_all = pct::get_od()
 od = od_all |> 
-  select(o = `Area of residence`, d = `Area of workplace`, all = `All categories: Method of travel to work`, bicycle = Bicycle, foot = `On foot`, car_driver = `Driving a car or van`, train = Train) |>
+  select(o = geo_code1, d = geo_code2, all, bicycle, foot, car_driver, train) |>
   filter(o %in% zones$geo_code & d %in% zones$geo_code, all > 19)
 summary(zones$geo_code %in% od$d)
 summary(zones$geo_code %in% od$o)
-
 
 # get route network data --------------------------------------------------
 
@@ -66,7 +71,7 @@ ways = st_intersection(res$osm_lines, region_ttwa)
 ways$highway = as.character(ways$highway)
 ways$highway[ways$railway == "rail"] = "rail"
 ways$highway = gsub("_link", "", x = ways$highway) |> 
-  gsub("motorway|primary|secondary", "road", x = .) |> 
+  gsub("motorway|primary|secondary", "road", x = _) |> 
   as.factor()
 ways = ways |> 
   select(highway, maxspeed, ref)
