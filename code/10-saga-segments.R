84bbdbd88525c405179906fc6c28e6a583be24e4
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-03-17 10:05:35 +0100

    prepares ch10 to tmap4

--- code/10-saga-segments.R
+++ code/10-saga-segments.R
@@ -27,5 +27,19 @@ tms2 = tms1 +
   tm_layout(legend.show = FALSE)
 tms = tmap_arrange(tms1, tms2)
 
+if (packageVersion("tmap") >= "4.0"){
+  tms1 = tm_shape(ndvi) +
+    tm_raster(col.scale = tm_scale_continuous(n = 7, values = "PRGn"),
+              col.legend = tm_legend(title = "NDVI")) +
+    tm_layout(frame = FALSE,
+              legend.frame = TRUE, legend.position = c("LEFT", "BOTTOM"),
+              legend.bg.color = "white")
+  tms2 = tms1 + 
+    tm_shape(ndvi_segments) +
+    tm_borders(col = "red", lwd = 0.5) +
+    tm_layout(legend.show = FALSE)
+  tms = tmap_arrange(tms1, tms2)
+}
+
 tmap_save(tms, filename = "figures/10-saga-segments.png", dpi = 150, width = 9,
           height = 4.75, units = "cm")
