b39c9256cebfa8aacdf310a0511989135e1aa531
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-04-19 10:04:42 +0200

    fixes app

--- apps/coffeeApp/app.R
+++ apps/coffeeApp/app.R
@@ -3,14 +3,13 @@ library(sf)
 library(shiny)
 library(spData)
 library(leaflet)
-library(tidyverse)
+library(dplyr)
 world_coffee = left_join(world, coffee_data)
 pal = colorNumeric(palette = "RdYlBu", domain = c(0, 4000))
 
 ui = fluidPage(
   sidebarPanel(
-    sliderInput("range", "Coffee Production", 0, 4000,
-                value = c(1000, 4000), step = 100),
+    sliderInput("range", "Coffee Production", 0, 4000, value = c(1000, 4000), step = 100),
     selectInput("year", "Year", c(2016, 2017)),
     checkboxInput("legend", "Show legend", FALSE)
   ),
@@ -21,7 +20,7 @@ ui = fluidPage(
 
 server = function(input, output, session) {
   
-  map_centre = st_centroid(world %>% filter(name_long == "Brazil")) %>% 
+  map_centre = st_centroid(world |> filter(name_long == "Brazil")) |> 
     st_coordinates()
   
   # This reactive expression returns a character string representing the selected variable
@@ -38,18 +37,18 @@ server = function(input, output, session) {
   
   output$map = renderLeaflet({
     # Things that do not change go here:
-    leaflet() %>% addTiles() %>%
-      setView(lng = map_centre[, "X"], map_centre[, "Y"], zoom = 2)
+    leaflet() |> addTiles() |>
+      setView(lng = map_centre[1], map_centre[2], zoom = 2)
   })
   
   # Changes to the map performed in an observer
   observe({
-    proxy = leafletProxy("map", data = filteredData()) %>% 
+    proxy = leafletProxy("map", data = filteredData()) |> 
       clearShapes()
     # Show or hide legend
-    proxy %>% clearControls() %>% addPolygons(fillColor = ~pal(Production))
+    proxy |> clearControls() |> addPolygons(fillColor = ~pal(Production))
     if (input$legend) {
-      proxy %>% addLegend(position = "bottomright",
+      proxy |> addLegend(position = "bottomright",
                           pal = pal, values = ~Production)
     }
   })
