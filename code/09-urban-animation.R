216a24235c46d534cd3488ff619effd998470615
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-06-24 19:08:18 +0200

    adds wip

--- code/09-urban-animation.R
+++ code/09-urban-animation.R
@@ -6,7 +6,8 @@ world2 = filter(world, continent != "Antarctica")
 m_save = tm_shape(world2) + 
   tm_polygons() +
   tm_shape(urban_agglomerations) +
-  tm_dots(size = "population_millions", title.size = "Population (m)", alpha = 0.5, col = "red") +
-  tm_facets(along = "year", free.coords = FALSE)
-tmap::tmap_animation(tm = m_save, filename = "/tmp/urban-animated.gif", width = 1200, height = 800)
+  tm_symbols(size = "population_millions", size.legend = tm_legend(title = "Population (m)"),
+             fill = "red", fill_alpha = 0.5) +
+  tm_facets(page = "year", free.coords = FALSE)
+tmap:::tmap_animation(tm = m_save, filename = "/tmp/urban-animated.gif", width = 1200, height = 800)
 magick::image_read("/tmp/urban-animated.gif")
