413d4471155ecfc2b629e9c704af00254a8cc5ed
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-07-22 16:59:46 +0200

    improves mapping

--- code/09-layout2.R
+++ code/09-layout2.R
@@ -8,5 +8,7 @@ map_nza = tm_shape(nz) +
 c1 = map_nza + tm_title('frame.lwd = 5', position = c("center", "bottom")) + tm_layout(frame.lwd = 5) 
 c2 = map_nza + tm_title('inner.margins = rep(0.2, 4)', position = c("center", "bottom")) + tm_layout(inner.margins = rep(0.2, 4))
 c3 = map_nza + tm_title('legend.show = FALSE', position = c("center", "bottom")) + tm_layout(legend.show = FALSE)
-c4 = map_nza + tm_title('legend.position =\n c("right", "bottom")', position = c("center", "bottom")) + tm_layout(legend.position = c("right", "bottom"))
+c4 = tm_shape(nz) +
+  tm_fill(fill = "Land_area") +
+  tm_borders() + tm_title('legend.position =\n c("right", "bottom")', position = c("center", "bottom")) + tm_layout(legend.position = c("right", "bottom"))
 tmap_arrange(c1, c2, c3, c4, nrow = 1)
