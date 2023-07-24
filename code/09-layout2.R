219ac17295ec4437e8b1307273b7a79a1e3997f1
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-07-24 10:27:40 +0200

    updates tmap code

--- code/09-layout2.R
+++ code/09-layout2.R
@@ -10,5 +10,5 @@ c2 = map_nza + tm_title('inner.margins = rep(0.2, 4)', position = c("center", "b
 c3 = map_nza + tm_title('legend.show = FALSE', position = c("center", "bottom")) + tm_layout(legend.show = FALSE)
 c4 = tm_shape(nz) +
   tm_fill(fill = "Land_area") +
-  tm_borders() + tm_title('legend.position =\n c("right", "bottom")', position = c("center", "bottom")) + tm_layout(legend.position = c("right", "bottom"))
+  tm_borders() + tm_title('legend.position =\n c("right", "bottom")', position = c("center", "top")) + tm_layout(legend.position = c("right", "bottom"))
 tmap_arrange(c1, c2, c3, c4, nrow = 1)
--- code/09-urban-animation.R
+++ code/09-urban-animation.R
@@ -8,6 +8,6 @@ m_save = tm_shape(world2) +
   tm_shape(urban_agglomerations) +
   tm_symbols(size = "population_millions", size.legend = tm_legend(title = "Population (m)"),
              fill = "red", fill_alpha = 0.5) +
-  tm_facets(page = "year", free.coords = FALSE)
+  tm_facets(by = "year", nrow = 1, ncol = 1, free.coords = FALSE)
 tmap:::tmap_animation(tm = m_save, filename = "/tmp/urban-animated.gif", width = 1200, height = 800)
 magick::image_read("/tmp/urban-animated.gif")
