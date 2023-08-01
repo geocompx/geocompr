53495d24b8e6bc829cb1299f99a3aec2b6ba6a66
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-08-01 11:03:16 +0200

    updates maps 05

--- code/05-extend-example.R
+++ code/05-extend-example.R
@@ -8,23 +8,11 @@ elev_poly = st_as_sf(as.polygons(elev, dissolve = FALSE))
 elev2_poly = st_as_sf(as.polygons(elev2, na.rm = FALSE, dissolve = FALSE))
 
 tm1 = tm_shape(elev_poly, bbox = elev2_poly) +
-  tm_polygons(col = "elev") +
+  tm_polygons(fill = "elev") +
   tm_layout(frame = FALSE, legend.show = FALSE)
 
 tm2 = tm_shape(elev2_poly) +
-  tm_polygons(col = "elev") +
+  tm_polygons(fill = "elev") +
   tm_layout(frame = FALSE, legend.show = FALSE)
 
 tmap_arrange(tm1, tm2, nrow = 1)
-
-if (packageVersion("tmap") >= "4.0"){
-  tm1 = tm_shape(elev_poly, bbox = elev2_poly) +
-    tm_polygons(fill = "elev") +
-    tm_layout(frame = FALSE, legend.show = FALSE)
-  
-  tm2 = tm_shape(elev2_poly) +
-    tm_polygons(fill = "elev") +
-    tm_layout(frame = FALSE, legend.show = FALSE)
-  
-  tmap_arrange(tm1, tm2, nrow = 1)
-}
--- code/05-us-regions.R
+++ code/05-us-regions.R
@@ -1,17 +1,3 @@
-# with base R ----
-# old_par = par()
-# breaks = c(1e5, 5e6, 1e7, 7e7, 2e8)
-# rdf = st_drop_geometry(regions)
-# us_states$region_pop = inner_join(select(us_states, REGION),
-#                                   select(rdf, Group.1, total_pop_15),
-#                                   by = c("REGION" = "Group.1")) |>
-#   pull(total_pop_15)
-# 
-# par(mfrow = c(1, 2))
-# plot(us_states[, "total_pop_15"], main = "US states", breaks = breaks, key.pos = NULL)
-# plot(regions[, "total_pop_15"], main = "US regions", breaks = breaks, key.pos = NULL)
-# par(old_par)
-# with tmap ----
 library(tmap)
 library(spData)
 library(dplyr)
@@ -28,11 +14,5 @@ us_facet = rbind(us_states_facet, regions_facet) |>
   st_cast("MULTIPOLYGON")
 
 tm_shape(us_facet) +
-  tm_polygons("total_pop_15", title = "Total population:") +
-  tm_facets(by = "Level", nrow = 1, drop.units = TRUE)
-
-if (packageVersion("tmap") >= "4.0"){
-  tm_shape(us_facet) +
-    tm_polygons("total_pop_15", fill.legend = tm_legend("Total population:")) +
-    tm_facets(by = "Level", nrows = 1, drop.units = TRUE)
-}
+  tm_polygons("total_pop_15", fill.legend = tm_legend("Total population:")) +
+  tm_facets(by = "Level", ncols = 2, drop.units = TRUE)
