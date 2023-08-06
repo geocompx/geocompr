348ce73e1a73cb5882a25a3fc5eab4496709f0b5
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-08-06 17:41:00 +0200

    updates 10 to tmap

--- code/10-saga-segments.R
+++ code/10-saga-segments.R
@@ -18,28 +18,16 @@ ndvi_segments = as.polygons(ndvi_srg$segments) |>
   st_as_sf()
 
 tms1 = tm_shape(ndvi) +
-  tm_raster(style = "cont", palette = "PRGn", title = "NDVI",
-            n = 7) +
-  tm_layout(frame = FALSE, legend.frame = TRUE, legend.position = c("LEFT", "BOTTOM"), legend.text.size = 0.4, legend.title.size = 0.5)
+  tm_raster(col.scale = tm_scale_continuous(n = 7, values = "PRGn"),
+            col.legend = tm_legend(title = "NDVI")) +
+  tm_layout(frame = FALSE,
+            legend.frame = TRUE, legend.position = c("LEFT", "BOTTOM"),
+            legend.bg.color = "white")
 tms2 = tms1 + 
   tm_shape(ndvi_segments) +
   tm_borders(col = "red", lwd = 0.5) +
   tm_layout(legend.show = FALSE)
 tms = tmap_arrange(tms1, tms2)
-
-if (packageVersion("tmap") >= "4.0"){
-  tms1 = tm_shape(ndvi) +
-    tm_raster(col.scale = tm_scale_continuous(n = 7, values = "PRGn"),
-              col.legend = tm_legend(title = "NDVI")) +
-    tm_layout(frame = FALSE,
-              legend.frame = TRUE, legend.position = c("LEFT", "BOTTOM"),
-              legend.bg.color = "white")
-  tms2 = tms1 + 
-    tm_shape(ndvi_segments) +
-    tm_borders(col = "red", lwd = 0.5) +
-    tm_layout(legend.show = FALSE)
-  tms = tmap_arrange(tms1, tms2)
-}
 
 tmap_save(tms, filename = "figures/10-saga-segments.png", dpi = 150, width = 9,
           height = 4.75, units = "cm")
