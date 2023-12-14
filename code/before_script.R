06a15dc4f5246118c538a34b3635d85200016e3d
nowosad <tupiszakaczki@gmail.com>
2023-12-14 12:26:49 +0100

    tests idea of automatic cropping lines

--- code/before_script.R
+++ code/before_script.R
@@ -33,5 +33,19 @@ knit_hooks$set(output = function(x, options) {
 set.seed(2017)
 options(digits = 3)
 options(dplyr.print_min = 4, dplyr.print_max = 4)
-# Hide proj4 warnings:
-options("rgdal_show_exportToProj4_warnings" = "none")
+
+# https://github.com/rstudio/rmarkdown-cookbook/commit/876bca3facedd30b8cc48cd9c1c86020d1e2adf9
+# save the build-in output hook
+hook_output = knitr::knit_hooks$get("output")
+# set a new output hook to truncate text output
+knitr::knit_hooks$set(output = function(x, options) {
+  if (!is.null(n <- options$out.lines)) {
+    x = knitr:::split_lines(x)
+    if (length(x) > n) {
+      # truncate the output
+      x = c(head(x, n), '....\n')
+    }
+    x = paste(x, collapse = '\n')
+  }
+  hook_output(x, options)
+})
