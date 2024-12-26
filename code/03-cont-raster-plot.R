7551f5db94f98b4146232c6a8033d68e2f9268f8
Jakub <Nowosad@users.noreply.github.com>
2024-12-26 10:26:32 +0100

    Update 03-cont-raster-plot.R

--- code/03-cont-raster-plot.R
+++ code/03-cont-raster-plot.R
@@ -8,12 +8,12 @@ colfunc2 = c("clay" = "brown", "silt" = "sandybrown", "sand" = "rosybrown")
 p1 = tm_shape(elev) + 
   tm_raster(col.scale = tm_scale_continuous(),
             col.legend = tm_legend(title = "",
-                                   position = tm_pos_auto_in(),
+                                   position = tm_pos_in(),
                                    bg.color = "white")) 
 
 p2 = tm_shape(grain) + 
   tm_raster(col.scale = tm_scale_categorical(values = colfunc2),
             col.legend = tm_legend(title = "",
-                                   position = tm_pos_auto_in(),
+                                   position = tm_pos_in(),
                                    bg.color = "white")) 
 tmap_arrange(p1, p2, nrow = 1)
